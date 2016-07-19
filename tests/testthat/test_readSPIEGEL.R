77b29788cec26f2fa41858f68afb744c5f18d2ec
Lkoppers <lars.koppers@tu-dortmund.de>
2016-07-19 15:58:46 +0200

    readSPIEGEL testthat

---
+++ tests/testthat/test_readSPIEGEL.R
@@ -0,0 +1,13 @@
+context("read SPIEGEL files")
+
+test_that("readSPIEGEL", {
+
+SPIEGEL2 <- readSPIEGEL(year=2012:2013)
+text2 <- readSPIEGEL(year=2012:2013, do.meta = FALSE, do.text = TRUE)
+meta2 <- readSPIEGEL(year=2012:2013, do.meta = TRUE, do.text = FALSE)
+
+load("SP_compare.Rdata")
+expect_equal(SPIEGEL2, SPIEGEL)
+expect_equal(text2, text)
+expect_equal(meta2, meta)
+})
