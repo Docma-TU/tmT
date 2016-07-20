779bbb32bc4057cea798923edd8836819562d3bd
Lkoppers <lars.koppers@tu-dortmund.de>
2016-07-20 13:34:08 +0200

    Fix test readSPIEGEL

--- tests/testthat/test_readSPIEGEL.R
+++ tests/testthat/test_readSPIEGEL.R
@@ -6,7 +6,11 @@ SPIEGEL2 <- readSPIEGEL(year=2012:2013)
 text2 <- readSPIEGEL(year=2012:2013, do.meta = FALSE, do.text = TRUE)
 meta2 <- readSPIEGEL(year=2012:2013, do.meta = TRUE, do.text = FALSE)
 
-load("tests/testthat/SP_compare.Rdata")
+
+
+
+
+load("Spiegel-Daten/SP_compare.Rdata")
 expect_equal(SPIEGEL2, SPIEGEL)
 expect_equal(text2, text)
 expect_equal(meta2, meta)
