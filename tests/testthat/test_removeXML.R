9b071ade93088eafa85a9d0988b539ebc0a9fc56
Lkoppers <lars.koppers@tu-dortmund.de>
2016-07-21 10:48:18 +0200

    encoding fix in removeXML

--- tests/testthat/test_removeXML.R
+++ tests/testthat/test_removeXML.R
@@ -4,9 +4,9 @@ test_that("removeXML", {
 Sys.setlocale('LC_ALL','C')
 
 text <- list("aba<vcs>ab</vcs>caa", "ab&dgv;abc", c("&auml;&ouml;&uuml;&szlig;&Ouml;&Auml;&Uuml;"), c("aa", "aab", "bc"))
-text2 <- list("aba<vcs>ab</vcs>caa", "ab&dgv;abc", c("הצüßײִÜ"), c("aa", "aab", "bc"))
+text2 <- list("aba<vcs>ab</vcs>caa", "ab&dgv;abc", iconv(c("הצüßײִÜ"), from="latin1", to="UTF-8"), c("aa", "aab", "bc"))
 
-expect_equal(removeXML(x=text2, umlaute = FALSE, u.type = c("normal", "html", "all")), list(c("aba ab caa"),c("ab&dgv;abc"),c("הצüßײִÜ"),c("aa",  "aab", "bc")))
+expect_equal(removeXML(x=text2, umlaute = FALSE, u.type = c("normal", "html", "all")), list(c("aba ab caa"),c("ab&dgv;abc"),iconv(c("הצüßײִÜ"), from="latin1", to="UTF-8") ,c("aa",  "aab", "bc")))
 
 exp1 <- list(c("aba ab caa"),c("ab&dgv;abc"),c("aeoeuessOeAeUe"),c("aa",  "aab", "bc"))
 exp2 <- list(c("aba ab caa"),c("ababc"),c("aeoeuessOeAeUe"),c("aa",  "aab", "bc"))
