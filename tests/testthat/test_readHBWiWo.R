7babd3fd39ed537628de8a11b6b5a8e4c0950dbc
Lkoppers <lars.koppers@tu-dortmund.de>
2016-07-21 16:36:05 +0200

    test HBWiWo add test=TRUE

--- tests/testthat/test_readHBWiWo.R
+++ tests/testthat/test_readHBWiWo.R
@@ -2,8 +2,8 @@ context("read HB or WiWo files")
 
 test_that("readHBWiWo", {
 HBWiWo2 <- readHBWiWo(file="data/HBWiWo1.txt", en=FALSE, test=TRUE)
-text2 <- readHBWiWo(file="data/HBWiWo1.txt", do.meta = FALSE, do.text = TRUE, en=FALSE)
-meta2 <- readHBWiWo(file="data/HBWiWo1.txt", do.meta = TRUE, do.text = FALSE, en=FALSE)
+text2 <- readHBWiWo(file="data/HBWiWo1.txt", do.meta = FALSE, do.text = TRUE, en=FALSE, test=TRUE)
+meta2 <- readHBWiWo(file="data/HBWiWo1.txt", do.meta = TRUE, do.text = FALSE, en=FALSE, test=TRUE)
 
 load("data/HBWiWo_compare.Rdata")
 expect_equal(HBWiWo2, HBWiWo)
@@ -11,9 +11,9 @@ expect_equal(text2, text)
 expect_equal(meta2, meta)
 
 
-HBWiWo2 <- readHBWiWo(file="data/HBWiWo2.txt", en=TRUE)
-text2 <- readHBWiWo(file="data/HBWiWo2.txt", do.meta = FALSE, do.text = TRUE, en=TRUE)
-meta2 <- readHBWiWo(file="data/HBWiWo2.txt", do.meta = TRUE, do.text = FALSE, en=TRUE)
+HBWiWo2 <- readHBWiWo(file="data/HBWiWo2.txt", en=TRUE, test=TRUE)
+text2 <- readHBWiWo(file="data/HBWiWo2.txt", do.meta = FALSE, do.text = TRUE, en=TRUE, test=TRUE)
+meta2 <- readHBWiWo(file="data/HBWiWo2.txt", do.meta = TRUE, do.text = FALSE, en=TRUE, test=TRUE)
 
 load("data/HBWiWo_en_compare.Rdata")
 expect_equal(HBWiWo2, HBWiWo)
