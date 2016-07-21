b5f704c7f243677425b834e078aa4aa832c5a5ce
Lkoppers <lars.koppers@tu-dortmund.de>
2016-07-21 10:23:49 +0200

    removeXML fix

--- tests/testthat/test_removeXML.R
+++ tests/testthat/test_removeXML.R
@@ -3,12 +3,16 @@ context("remove XML tags and change umlauts style")
 test_that("removeXML", {
 Sys.setlocale('LC_ALL','C')
 
-text <- list("aba<vcs>ab</vcs>caa", "ab&dgv;abc", c("&Auml; &Ouml; &Uuml; &auml; &ouml; &uuml; &szlig;"), c("aa", "aab", "bc"))
+text <- list("aba<vcs>ab</vcs>caa", "ab&dgv;abc", c("&auml;&ouml;&uuml;&szlig;&Ouml;&Auml;&Uuml;"), c("aa", "aab", "bc"))
 text2 <- list("aba<vcs>ab</vcs>caa", "ab&dgv;abc", c("äöüßÖÄÜ"), c("aa", "aab", "bc"))
 
 expect_equal(removeXML(x=text2, umlaute = FALSE, u.type = c("normal", "html", "all")), list(c("aba ab caa"),c("ab&dgv;abc"),c("äöüßÖÄÜ"),c("aa",  "aab", "bc")))
 
-#expect_equal(removeXML(x=text2, umlaute = TRUE, u.type = "normal"), list(c("aba ab caa"),c("ab&dgv;abc"),c("aeoeuessOeAeUe"),c("aa",  "aab", "bc")))
-
+exp1 <- list(c("aba ab caa"),c("ab&dgv;abc"),c("aeoeuessOeAeUe"),c("aa",  "aab", "bc"))
+exp2 <- list(c("aba ab caa"),c("ababc"),c("aeoeuessOeAeUe"),c("aa",  "aab", "bc"))
+expect_equal(removeXML(x=text2, umlaute = TRUE, u.type = "normal"), exp1)
+expect_equal(removeXML(x=text , umlaute = TRUE, u.type = "html"), exp2)
+expect_equal(removeXML(x=text2, umlaute = TRUE, u.type = "all"), exp2)
+expect_equal(removeXML(x=text , umlaute = TRUE, u.type = "all"), exp2)
 
 })
