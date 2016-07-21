6a3fe37b23c5b96729964af1ead824a676ea787d
Lkoppers <lars.koppers@tu-dortmund.de>
2016-07-21 16:03:49 +0200

    test readHBWiWo and readSZ

--- tests/testthat/test_readHBWiWo.R
+++ tests/testthat/test_readHBWiWo.R
@@ -1,19 +1,19 @@
 context("read HB or WiWo files")
 
 test_that("readHBWiWo", {
-HBWiWo2 <- readHBWiWo(file="../tests/testthat/data/HBWiWo1.txt", en=FALSE)
-text2 <- readHBWiWo(file="../tests/testthat/data/HBWiWo1.txt", do.meta = FALSE, do.text = TRUE, en=FALSE)
-meta2 <- readHBWiWo(file="../tests/testthat/data/HBWiWo1.txt", do.meta = TRUE, do.text = FALSE, en=FALSE)
+HBWiWo2 <- readHBWiWo(file="../data/HBWiWo1.txt", en=FALSE)
+text2 <- readHBWiWo(file="../data/HBWiWo1.txt", do.meta = FALSE, do.text = TRUE, en=FALSE)
+meta2 <- readHBWiWo(file="../data/HBWiWo1.txt", do.meta = TRUE, do.text = FALSE, en=FALSE)
 
-load("tests/testthat/data/HBWiWo_compare.Rdata")
+load("data/HBWiWo_compare.Rdata")
 expect_equal(HBWiWo2, HBWiWo)
 expect_equal(text2, text)
 expect_equal(meta2, meta)
 
 
-HBWiWo2 <- readHBWiWo(file="../tests/testthat/data/HBWiWo2.txt", en=TRUE)
-text2 <- readHBWiWo(file="../tests/testthat/data/HBWiWo2.txt", do.meta = FALSE, do.text = TRUE, en=TRUE)
-meta2 <- readHBWiWo(file="../tests/testthat/data/HBWiWo2.txt", do.meta = TRUE, do.text = FALSE, en=TRUE)
+HBWiWo2 <- readHBWiWo(file="../data/HBWiWo2.txt", en=TRUE)
+text2 <- readHBWiWo(file="../data/HBWiWo2.txt", do.meta = FALSE, do.text = TRUE, en=TRUE)
+meta2 <- readHBWiWo(file="../data/HBWiWo2.txt", do.meta = TRUE, do.text = FALSE, en=TRUE)
 
 load("tests/testthat/data/HBWiWo_en_compare.Rdata")
 expect_equal(HBWiWo2, HBWiWo)
--- tests/testthat/test_readSZ.R
+++ tests/testthat/test_readSZ.R
@@ -2,11 +2,11 @@ context("read SZ files")
 
 test_that("readSZ", {
 
-SZ2 <- readSZ(file=c("tests/testthat/data/SZ/SZ1.txt","tests/testthat/data/SZ/SZ2.txt"), folder="tests/testthat/data/SZ")
-text2 <- readSZ(file=c("tests/testthat/data/SZ/SZ1.txt","tests/testthat/data/SZ/SZ2.txt"), folder="tests/testthat/data/SZ", do.meta = FALSE, do.text = TRUE)
-meta2 <- readSZ(file=c("tests/testthat/data/SZ/SZ1.txt","tests/testthat/data/SZ/SZ2.txt"), folder="tests/testthat/data/SZ", do.meta = TRUE, do.text = FALSE)
+SZ2 <- readSZ(file=c("data/SZ/SZ1.txt","tests/testthat/data/SZ/SZ2.txt"), folder="tests/testthat/data/SZ")
+text2 <- readSZ(file=c("data/SZ/SZ1.txt","tests/testthat/data/SZ/SZ2.txt"), folder="tests/testthat/data/SZ", do.meta = FALSE, do.text = TRUE)
+meta2 <- readSZ(file=c("data/SZ/SZ1.txt","tests/testthat/data/SZ/SZ2.txt"), folder="tests/testthat/data/SZ", do.meta = TRUE, do.text = FALSE)
 
-load("tests/testthat/data/SZ_compare.Rdata")
+load("data/SZ_compare.Rdata")
 expect_equal(SZ2, SZ)
 expect_equal(text2, text)
 expect_equal(meta2, meta)
