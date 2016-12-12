3a6d636b0d62003bcdd2dd0ae7d368f14ee0c0bf
JonasRieger <riegerjonas@gmx.de>
2016-12-12 16:19:50 +0100

    two new functions and roxygenise
    added functions read.summary and show.articles and revised some
    documentations by roxygen

---
+++ R/read.summary.R
@@ -0,0 +1,59 @@
+#' Print Summarys of Datsets.
+#'
+#' Prints a summary of a given dataset, which should be a result of a read-funtion.
+#'
+#'
+#' @param data A list of corpus data as a result of a read-function.
+#' @param metamult Logical: Should the function return a summary of data$metamult?
+#' @return A print in the console.
+#' @author Jonas Rieger (<riegerjonas@@gmx.de>)
+#' @keywords manip
+#' @examples
+#'
+#' @export read.summary
+#'
+read.summary <- function(data, metamult = "metamult" %in% colnames(data)){
+  stopifnot(is.list(data), all(c("text", "meta") %in% names(data)),
+            is.character(data$text), is.data.frame(data$meta),
+            is.logical(metamult), length(metamult) == 1)
+  # data$text:
+  n.text <- length(data$text)
+  na.text <- sum(is.na(data$text))
+  # data$meta:
+  cols <- colnames(data$meta)
+  n.meta <- nrow(data$meta)
+  na.meta <- sapply(data$meta, function(x) sum(is.na(x)))
+  # print summary:
+  cat("number of observations:\n")
+  print(data.frame(n.text, n.meta), row.names = FALSE)
+  cat("\nNAs in text:\n")
+  print(data.frame(abs = na.text, rel = na.text/n.text), row.names = FALSE)
+  cat("\nNAs in meta:\n")
+  print(cbind(abs = na.meta, rel = na.meta/n.meta))
+  nextprint <- paste(paste0(rep("-", 70), collapse = ""), "\n\n")
+  # print date-range:
+  cat(paste(nextprint, "range of date:\n"))
+  if ("date" %in% cols){
+    print(range(data$meta$date))
+  }
+  if ("datum" %in% cols){
+    print(range(data$meta$datum))
+  }
+  # print tables of candidates
+  candidates <- c("resource", "downloadDate")
+  for (i in candidates){
+    if (i %in% cols){
+      tab <- table(data$meta[, i])
+      cat(paste0(nextprint, i, ":\n"))
+      print(cbind(abs = tab, rel = tab/n.meta))
+    }
+  }
+  if (metamult){
+    n.metamult <- nrow(data$metamult)
+    na.metamult <- sapply(data$metamult, function(x) sum(is.na(x)))
+    cat(paste0(nextprint, "metamult:\n",
+               "number of observations: ", n.metamult, "\n",
+               "NAs in metamult:\n"))
+    print(cbind(abs = na.metamult, rel = na.metamult/n.metamult))
+  }
+}
---
+++ R/show.articles.R
@@ -0,0 +1,30 @@
+#' Export Readable Meta-Data of Articles.
+#'
+#' Exports requested meta-data of articles for given ids.
+#'
+#'
+#' @param meta A data.frame of meta-data as a result of a read-function.
+#' @param file Filename for the export.
+#' @param id Vector of characters including article ids.
+#' @param cols vector of characters including the requested colnames of meta.
+#' @param csv.ger Logical: Should the function use write.csv2 instead of write.csv?
+#' @return No output in R, writes a csv (or csv2) including the meta-data of the
+#' requested articles.
+#' @author Jonas Rieger (<riegerjonas@@gmx.de>)
+#' @keywords manip
+#' @examples
+#'
+#' @export show.articles
+#' 
+show.articles <- function(meta, file, id = meta$id, cols = colnames(meta),
+                          csv.ger = FALSE){
+  stopifnot(is.data.frame(meta), is.character(file), length(file) == 1,
+            is.character(id), all(id %in% meta$id),
+            is.character(cols), all(cols %in% colnames(meta)))
+  if (csv.ger){
+    write.csv2(meta[meta$id %in% id, cols], file = file, row.names = FALSE)
+  }
+  else {
+    write.csv(meta[meta$id %in% id, cols], file = file, row.names = FALSE)
+  }
+}
