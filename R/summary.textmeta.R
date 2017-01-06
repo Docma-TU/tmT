cbf32c855fb6f09c714fce948f697065b7618a5c
JonasRieger <riegerjonas@gmx.de>
2017-01-06 14:33:07 +0100

    summary.textmeta maintenance

--- R/summary.textmeta.R
+++ R/summary.textmeta.R
@@ -1,41 +1,41 @@
 #' Print Summarys of Objects of class "textmeta".
 #'
-#' Prints a summary of a given dataset, which should be of class "textmeta".
+#' Prints a summary of a given Dataset, which should be of class "textmeta".
 #'
 #'
-#' @param data A list of corpus data of class "textmeta".
+#' @param object A list of corpus data of class "textmeta".
 #' @param list.names character: Which list.elements should be summarized?
-#' @return The Dataset itself.
+#' @return The dataset itself.
 #' @author Jonas Rieger (<riegerjonas@@gmx.de>)
 #' @keywords manip
 #' @examples
 #'
 #' @export summary.textmeta
 #'
-summary.textmeta <- function(data, list.names = names(data)){
-  stopifnot(is.list(data), is.character(list.names),
-            all(list.names %in% names(data)),
-            ifelse("text" %in% list.names && length(data$text) > 0,
-                   is.list(data$text) || is.character(data$text), TRUE),
-            ifelse("meta" %in% list.names && !is.null(nrow(data$meta)),
-                   is.data.frame(data$meta), TRUE),
-            ifelse("metamult" %in% list.names && !is.null(nrow(data$metamult)),
-                   is.list(data$metamult), TRUE))
+summary.textmeta <- function(object, list.names = names(object), ...){
+  stopifnot(is.list(object), is.character(list.names),
+            all(list.names %in% names(object)),
+            ifelse("text" %in% list.names && length(object$text) > 0,
+                   is.list(object$text) || is.character(object$text), TRUE),
+            ifelse("meta" %in% list.names && !is.null(nrow(object$meta)),
+                   is.object.frame(object$meta), TRUE),
+            ifelse("metamult" %in% list.names && !is.null(nrow(object$metamult)),
+                   is.list(object$metamult), TRUE))
   nextprint <- paste(paste0(rep("-", 70), collapse = ""), "\n\n")
-  # data$text:
-  if ("text" %in% list.names && length(data$text) > 0){
-    n.text <- length(data$text)
-    na.text <- sum(is.na(data$text))
+  # object$text:
+  if ("text" %in% list.names && length(object$text) > 0){
+    n.text <- length(object$text)
+    na.text <- sum(is.na(object$text))
     # print:
     cat(paste("number of observations text:", n.text, "\n"))
     cat("\nNAs in text:\n")
-    print(data.frame(abs = na.text, rel = na.text/n.text), row.names = FALSE)
+    print(object.frame(abs = na.text, rel = na.text/n.text), row.names = FALSE)
   }
-  # data$meta:
-  if ("meta" %in% list.names && !is.null(nrow(data$meta))){
-    cols <- colnames(data$meta)
-    n.meta <- nrow(data$meta)
-    na.meta <- sapply(data$meta, function(x) sum(is.na(x)))
+  # object$meta:
+  if ("meta" %in% list.names && !is.null(nrow(object$meta))){
+    cols <- colnames(object$meta)
+    n.meta <- nrow(object$meta)
+    na.meta <- sapply(object$meta, function(x) sum(is.na(x)))
     # print:
     cat(paste0(nextprint, "number of observations meta: ", n.meta, "\n"))
     cat("\nNAs in meta:\n")
@@ -43,28 +43,28 @@ summary.textmeta <- function(data, list.names = names(data)){
     # print date-range:
     cat(paste(nextprint, "range of date:\n"))
     if ("date" %in% cols){
-      print(range(data$meta$date))
+      print(range(object$meta$date))
     }
     if ("datum" %in% cols){
-      print(range(data$meta$datum))
+      print(range(object$meta$datum))
     }
     # print tables of candidates
     candidates <- c("resource", "downloadDate")
     for (i in candidates){
       if (i %in% cols){
-        tab <- table(data$meta[, i])
+        tab <- table(object$meta[, i])
         cat(paste0(nextprint, i, ":\n"))
         print(cbind(abs = tab, rel = tab/n.meta))
       }
     }
   }
-  if ("metamult" %in% list.names && !is.null(nrow(data$metamult))){
-    n.metamult <- nrow(data$metamult)
-    na.metamult <- sapply(data$metamult, function(x) sum(is.na(x)))
+  if ("metamult" %in% list.names && !is.null(nrow(object$metamult))){
+    n.metamult <- nrow(object$metamult)
+    na.metamult <- sapply(object$metamult, function(x) sum(is.na(x)))
     cat(paste0(nextprint, "metamult:\n",
                "number of observations: ", n.metamult, "\n",
                "NAs in metamult:\n"))
     print(cbind(abs = na.metamult, rel = na.metamult/n.metamult))
   }
-  invisible(data)
+  invisible(object)
 }
