d42f21bda92ee961834ae66479149d60f28477dd
Lkoppers <lars.koppers@tu-dortmund.de>
2017-01-17 17:03:53 +0100

    Update readNexis, help-Page readHBWiWo remove removeMISC and removeTAG

--- R/readHBWiWo.R
+++ R/readHBWiWo.R
@@ -3,8 +3,8 @@
 #' Reads the XML-files from the HB WiWo corpus and seperates the text and meta
 #' data.
 #'
-#' @param path Path where the data files are.
-#' @param file Names of the XML files.
+#' @param path Character string with Path where the data files are.
+#' @param file Character string with names of the XML files.
 #' @param do.meta Logical: Should the algorithm collect meta data?
 #' @param do.text Logical: Should the algorithm collect text data?
 #' @return \item{meta}{ id quelle datum titel abstract UB dachzeile}
@@ -17,131 +17,131 @@
 #' @export readHBWiWo
 #'
 readHBWiWo <- function(path = getwd(), file = list.files(path=path, pattern="*.xml", full.names=F, recursive=TRUE), do.meta = TRUE, do.text = TRUE){
-  stopifnot(is.character(file), is.character(path),
-            is.logical(do.meta), is.logical(do.text),
-            length(do.meta) == 1, length(do.text) == 1, length(path) == 1)
-  text <- NULL
-  meta <- NULL
-  metamult <- NULL
-  for(i in 1:length(file)){
-    (print(file[i]))
-    article <- readLines(con = paste(path,file[i], sep="/"), encoding = "latin1")
-    article <- gsub(pattern = "&quot;", replacement = "\"",article)
-    article <- gsub(pattern = "&amp;", replacement = "&",article)
-    article <- gsub(pattern = "&apos;", replacement = "\'",article)
-    lines <- grep(pattern = "</Dokument>|</Document>", article)
-    lines <- cbind(c(1,lines[-length(lines)]),lines)
-    article <- apply(lines, 1, function(x)paste(article[x[1]:x[2]], collapse = " "))
-    id <- stringr::str_extract(article, "ID=\"(.*?)\"")
-    id <- gsub(pattern="ID=|\"", replacement="", x=id)
+    stopifnot(is.character(file), is.character(path),
+              is.logical(do.meta), is.logical(do.text),
+              length(path) == 1, length(do.meta) == 1, length(do.text) == 1)
+    text <- NULL
+    meta <- NULL
+    metamult <- NULL
+    for(i in 1:length(file)){
+        (print(file[i]))
+        article <- readLines(con = paste(path,file[i], sep="/"), encoding = "latin1")
+           article <- gsub(pattern = "&quot;", replacement = "\"",article)
+           article <- gsub(pattern = "&amp;", replacement = "&",article)
+           article <- gsub(pattern = "&apos;", replacement = "\'",article)
+           lines <- grep(pattern = "</Dokument>|</Document>", article)
+           lines <- cbind(c(1,lines[-length(lines)]),lines)
+           article <- apply(lines, 1, function(x)paste(article[x[1]:x[2]], collapse = " "))
+           id <- stringr::str_extract(article, "ID=\"(.*?)\"")
+           id <- gsub(pattern="ID=|\"", replacement="", x=id)
 
-    if(do.meta){
-        source <- stringr::str_extract(article, "<Source>(.*?)</Source>|<Quelle>(.*?)</Quelle>")
-        source <- removeXML(source)
-        date <- stringr::str_extract(article, "<Date>(.*?)</Date>|<Datum>(.*?)</Datum>")
-        date <- as.Date(removeXML(date))
-        title <- stringr::str_extract(article, "<Title>(.*?)</Title>|<Titel>(.*?)</Titel>")
-        title <- removeXML(title)
-        abstract <- stringr::str_extract(article, "<Abstract>(.*?)</Abstract>")
-        abstract <- removeXML(abstract)
-        ub <- stringr::str_extract(article, "<UB>(.*?)</UB>")
-        ub <- removeXML(ub)
-        dachzeile <- stringr::str_extract(article, "<DZ>(.*?)</DZ>")
-        dachzeile <- removeXML(dachzeile)
+           if(do.meta){
+               source <- stringr::str_extract(article, "<Source>(.*?)</Source>|<Quelle>(.*?)</Quelle>")
+               source <- removeXML(source)
+               date <- stringr::str_extract(article, "<Date>(.*?)</Date>|<Datum>(.*?)</Datum>")
+               date <- as.Date(removeXML(date))
+               title <- stringr::str_extract(article, "<Title>(.*?)</Title>|<Titel>(.*?)</Titel>")
+               title <- removeXML(title)
+               abstract <- stringr::str_extract(article, "<Abstract>(.*?)</Abstract>")
+               abstract <- removeXML(abstract)
+               ub <- stringr::str_extract(article, "<UB>(.*?)</UB>")
+               ub <- removeXML(ub)
+               dachzeile <- stringr::str_extract(article, "<DZ>(.*?)</DZ>")
+               dachzeile <- removeXML(dachzeile)
 
-        company <- stringr::str_extract_all(article, "<Company>(.*?)</Company>|<Firma>(.*?)</Firma>")
-        names(company) <- id
-        tmp <- rep(names(company), lengths(company))
-        company <- unlist(company)
-        names(company) <- tmp
-        company <- trimws(gsub(pattern="<Company>|</Company>|<Firma>|</Firma>", replacement="", x=company))
+               company <- stringr::str_extract_all(article, "<Company>(.*?)</Company>|<Firma>(.*?)</Firma>")
+               names(company) <- id
+               tmp <- rep(names(company), lengths(company))
+               company <- unlist(company)
+               names(company) <- tmp
+               company <- trimws(gsub(pattern="<Company>|</Company>|<Firma>|</Firma>", replacement="", x=company))
 
-        country <- stringr::str_extract_all(article, "<Country>(.*?)</Country>|<Land>(.*?)</Land>")
-        names(country) <- id
-        tmp <- rep(names(country), lengths(country))
-        country <- unlist(country)
-        names(country) <- tmp
-        country <- trimws(gsub(pattern="<Country>|</Country>|<Land>|</Land>", replacement="", x=country))
+               country <- stringr::str_extract_all(article, "<Country>(.*?)</Country>|<Land>(.*?)</Land>")
+               names(country) <- id
+               tmp <- rep(names(country), lengths(country))
+               country <- unlist(country)
+               names(country) <- tmp
+               country <- trimws(gsub(pattern="<Country>|</Country>|<Land>|</Land>", replacement="", x=country))
 
-        industry <- stringr::str_extract_all(article, "<Industry>(.*?)</Industry>|<Industrie>(.*?)</Industrie>")
-        names(industry) <- id
-        tmp <- rep(names(industry), lengths(industry))
-        industry <- unlist(industry)
-        names(industry) <- tmp
-        industry <- trimws(gsub(pattern="<Industry>|</Industry>|<Industrie>|</Industrie>", replacement="", x=industry))
+               industry <- stringr::str_extract_all(article, "<Industry>(.*?)</Industry>|<Industrie>(.*?)</Industrie>")
+               names(industry) <- id
+               tmp <- rep(names(industry), lengths(industry))
+               industry <- unlist(industry)
+               names(industry) <- tmp
+               industry <- trimws(gsub(pattern="<Industry>|</Industry>|<Industrie>|</Industrie>", replacement="", x=industry))
 
-        author <- stringr::str_extract_all(article, "<Author>(.*?)</Author>|<Autor>(.*?)</Autor>")
-        names(author) <- id
-        tmp <- rep(names(author), lengths(author))
-        author <- unlist(author)
-        names(author) <- tmp
-        author <- trimws(gsub(pattern="<Author>|</Author>|<Autor>|</Autor>", replacement="", x=author))
+               author <- stringr::str_extract_all(article, "<Author>(.*?)</Author>|<Autor>(.*?)</Autor>")
+               names(author) <- id
+               tmp <- rep(names(author), lengths(author))
+               author <- unlist(author)
+               names(author) <- tmp
+               author <- trimws(gsub(pattern="<Author>|</Author>|<Autor>|</Autor>", replacement="", x=author))
 
-        category <- stringr::str_extract_all(article, "<Category>(.*?)</Category>|<Rubrik>(.*?)</Rubrik>")
-        names(category) <- id
-        tmp <- rep(names(category), lengths(category))
-        category <- unlist(category)
-        names(category) <- tmp
-        category <- trimws(gsub(pattern="<Category>|</Category>|<Rubrik>|</Rubrik>", replacement="", x=category))
+               category <- stringr::str_extract_all(article, "<Category>(.*?)</Category>|<Rubrik>(.*?)</Rubrik>")
+               names(category) <- id
+               tmp <- rep(names(category), lengths(category))
+               category <- unlist(category)
+               names(category) <- tmp
+               category <- trimws(gsub(pattern="<Category>|</Category>|<Rubrik>|</Rubrik>", replacement="", x=category))
 
-        klassifikation <- stringr::str_extract_all(article, "<Klassifikation>(.*?)</Klassifikation>")
-        names(klassifikation) <- id
-        tmp <- rep(names(klassifikation), lengths(klassifikation))
-        klassifikation <- unlist(klassifikation)
-        names(klassifikation) <- tmp
-        klassifikation <- trimws(gsub(pattern="<Klassifikation>|</Klassifikation>", replacement="", x=klassifikation))
+               klassifikation <- stringr::str_extract_all(article, "<Klassifikation>(.*?)</Klassifikation>")
+               names(klassifikation) <- id
+               tmp <- rep(names(klassifikation), lengths(klassifikation))
+               klassifikation <- unlist(klassifikation)
+               names(klassifikation) <- tmp
+               klassifikation <- trimws(gsub(pattern="<Klassifikation>|</Klassifikation>", replacement="", x=klassifikation))
 
-        thema <- stringr::str_extract_all(article, "<Thema>(.*?)</Thema>")
-        names(thema) <- id
-        tmp <- rep(names(thema), lengths(thema))
-        thema <- unlist(thema)
-        names(thema) <- tmp
-        thema <- trimws(gsub(pattern="<Thema>|</Thema>", replacement="", x=thema))
+               thema <- stringr::str_extract_all(article, "<Thema>(.*?)</Thema>")
+               names(thema) <- id
+               tmp <- rep(names(thema), lengths(thema))
+               thema <- unlist(thema)
+               names(thema) <- tmp
+               thema <- trimws(gsub(pattern="<Thema>|</Thema>", replacement="", x=thema))
 
-        sachgruppe <- stringr::str_extract_all(article, "<Sachgruppe>(.*?)</Sachgruppe>")
-        names(sachgruppe) <- id
-        tmp <- rep(names(sachgruppe), lengths(sachgruppe))
-        sachgruppe <- unlist(sachgruppe)
-        names(sachgruppe) <- tmp
-        sachgruppe <- trimws(gsub(pattern="<Sachgruppe>|</Sachgruppe>", replacement="", x=sachgruppe))
+               sachgruppe <- stringr::str_extract_all(article, "<Sachgruppe>(.*?)</Sachgruppe>")
+               names(sachgruppe) <- id
+               tmp <- rep(names(sachgruppe), lengths(sachgruppe))
+               sachgruppe <- unlist(sachgruppe)
+               names(sachgruppe) <- tmp
+               sachgruppe <- trimws(gsub(pattern="<Sachgruppe>|</Sachgruppe>", replacement="", x=sachgruppe))
 
-        serie <- stringr::str_extract_all(article, "<Serie>(.*?)</Serie>")
-        names(serie) <- id
-        tmp <- rep(names(serie), lengths(serie))
-        serie <- unlist(serie)
-        names(serie) <- tmp
-        serie <- trimws(gsub(pattern="<Serie>|</Serie>", replacement="", x=serie))
+               serie <- stringr::str_extract_all(article, "<Serie>(.*?)</Serie>")
+               names(serie) <- id
+               tmp <- rep(names(serie), lengths(serie))
+               serie <- unlist(serie)
+               names(serie) <- tmp
+               serie <- trimws(gsub(pattern="<Serie>|</Serie>", replacement="", x=serie))
 
-        person <- stringr::str_extract_all(article, "<Person>(.*?)</Person>")
-        names(person) <- id
-        tmp <- rep(names(person), lengths(person))
-        person <- unlist(person)
-        names(person) <- tmp
-        person <- trimws(gsub(pattern="<Person>|</Person>", replacement="", x=person))
+               person <- stringr::str_extract_all(article, "<Person>(.*?)</Person>")
+               names(person) <- id
+               tmp <- rep(names(person), lengths(person))
+               person <- unlist(person)
+               names(person) <- tmp
+               person <- trimws(gsub(pattern="<Person>|</Person>", replacement="", x=person))
 
-        mData <- data.frame(id, source, date, title, abstract, ub, dachzeile,
-                            stringsAsFactors = FALSE)
-        meta <- rbind(meta, mData)
-        metamult$person <- c(metamult$person, person)
-        metamult$company <- c(metamult$company, company)
-        metamult$industry <- c(metamult$industry, industry)
-        metamult$country <- c(metamult$country, country)
-        metamult$author <- c(metamult$author, author)
-        metamult$category <- c(metamult$category, category)
-        metamult$klassifikation <- c(metamult$klassifikation, klassifikation)
-        metamult$thema <- c(metamult$thema, thema)
-        metamult$sachgruppe <- c(metamult$sachgruppe, sachgruppe)
-        metamult$serie <- c(metamult$serie, serie)
+               mData <- data.frame(id, source, date, title, abstract, ub, dachzeile,
+                                   stringsAsFactors = FALSE)
+               meta <- rbind(meta, mData)
+               metamult$person <- c(metamult$person, person)
+               metamult$company <- c(metamult$company, company)
+               metamult$industry <- c(metamult$industry, industry)
+               metamult$country <- c(metamult$country, country)
+               metamult$author <- c(metamult$author, author)
+               metamult$category <- c(metamult$category, category)
+               metamult$klassifikation <- c(metamult$klassifikation, klassifikation)
+               metamult$thema <- c(metamult$thema, thema)
+               metamult$sachgruppe <- c(metamult$sachgruppe, sachgruppe)
+               metamult$serie <- c(metamult$serie, serie)
+           }
+           if(do.text){
+               text_new <- stringr::str_extract(article, "<Text>(.*?)</Text>")
+               text_new <- trimws(gsub(pattern="<Text>|</Text>", replacement="", x=text_new))
+               names(text_new) <- id
+               text <- as.list(c(text, text_new))
+           }
     }
-    if(do.text){
-        text_new <- stringr::str_extract(article, "<Text>(.*?)</Text>")
-        text_new <- trimws(gsub(pattern="<Text>|</Text>", replacement="", x=text_new))
-        names(text_new) <- id
-        text <- as.list(c(text, text_new))
-    }
-}
 #  text[is.na(text)] <- meta$abstract[is.na(text)]
-  res <- list("meta" = meta, "text" = text, "metamult" = metamult)
-  class(res) <- "textmeta"
-  summary(res)
+    res <- list("meta" = meta, "text" = text, "metamult" = metamult)
+    class(res) <- "textmeta"
+    summary(res)
 }
--- R/readNexis.R
+++ R/readNexis.R
@@ -4,6 +4,7 @@
 #' data.
 #'
 #'
+#' @param path Character string with Path where the data files are.
 #' @param file Character string with names of the XML files.
 #' @param do.meta Logical: Should the algorithm collect meta data?
 #' @param do.text Logical: Should the algorithm collect text data?
@@ -16,68 +17,65 @@
 #'
 #' @export readNexis
 #'
-readNexis <- function (file, do.meta = TRUE, do.text = TRUE, encoding = "utf-8"){
-  stopifnot(is.character(file), is.logical(do.meta), is.logical(do.text),
-            is.character(encoding), length(do.meta) == 1, length(do.text) == 1,
-            length(encoding) == 1)
-  text <- NULL
-  meta <- NULL
-  for (i in 1:length(file)) {
-    (print(file[i]))
-    lines <- NULL
+readNexis <- function(path = getwd(), file = list.files(path=path, pattern="*.xml", full.names=F, recursive=TRUE), do.meta = TRUE, do.text = TRUE, encoding = "utf-8"){
+    stopifnot(is.character(file), is.character(path),
+              is.logical(do.meta), is.logical(do.text), is.character(encoding),
+              length(path) == 1, length(do.meta) == 1, length(do.text) == 1, length(encoding) == 1)
+    text <- NULL
+    meta <- NULL
+    metamult <- NULL
+    for (i in seq_along(file)) {
+        (print(file[i]))
 
-    try(openfile <- file(file[i], open = "rt", encoding = encoding))
-    try(lines <- readLines(con = openfile))
-    try(close(openfile))
-    if (length(lines) == 0) {
-      next
+        article <- readLines(con = paste(path,file[i], sep="/"), encoding = encoding)
+        downloadDate <- as.character(na.omit(stringr::str_extract(article, "timestamp=(.*?)>")))
+        lines <- grep(pattern = "</record>", article)
+        lines <- cbind(c(1,lines[-length(lines)]),lines)
+        article <- apply(lines, 1, function(x)paste(article[x[1]:x[2]], collapse = " "))
+        article <-  stringr::str_extract(article, "<record>(.*?)</record>")
+
+        id <- stringr::str_extract(article, "<record_id>(.*?)</record_id>")
+        id <- removeXML(id)
+
+        if (do.meta) {
+            url <- stringr::str_extract(article, "<source_id>(.*?)</source_id>")
+            url <- removeXML(url)
+
+            date <- stringr::str_extract(article, "<publishDate>(.*?)</publishDate>")
+            date <- removeXML(date)
+            date <- as.Date(date, format = "%Y%m%d")
+
+            page <-  stringr::str_extract(article, "<series2>(.*?)</series2>")
+            page <- removeXML(page)
+
+            resource <-  stringr::str_extract(article, "<series>(.*?)</series>")
+            resource <- removeXML(resource)
+
+            author <-  stringr::str_extract(article, "<author>(.*?)</author>")
+            author <- removeXML(author)
+
+            leadtext <-  stringr::str_extract(article, "<leadtext>(.*?)</leadtext>")
+            leadtext <- removeXML(leadtext)
+            leadtext <- gsub(pattern = "^\\s+|\\s+$|Original Gesamtseiten-PDF", replacement = "", leadtext, perl = TRUE)
+
+            downloadDate <- stringr::str_extract(downloadDate, "[0-9-]+")
+            downloadDate <- as.Date(downloadDate, format = "%Y-%m-%d")
+            downloadDate <- rep(downloadDate, times = length(id))
+
+            mData <- data.frame(id, url, date, page, resource, author, leadtext,
+                                downloadDate, stringsAsFactors = FALSE)
+            meta <- rbind(meta, mData)
+        }
+        if (do.text) {
+            text_new <-  stringr::str_extract(article, "(<fulltext>(.*?)</fulltext>)|(<fulltext(.*?)/>)")
+            text_new <- removeXML(text_new)
+            text_new <- gsub(pattern = "^\\s+|\\s+$|Original Gesamtseiten-PDF", replacement = "", text_new, perl = TRUE)
+    #        text <- c(text, text_new)
+            names(text_new) <- id
+               text <- as.list(c(text, text_new))
+        }
     }
-
-    fulldata <- paste(lines, collapse = " ")
-    downloadDate <- str_extract(fulldata, "timestamp=(.*?)>")
-    fulldata <-  str_extract_all(fulldata, "<record>(.*?)</record>")[[1]]
-
-    id <-  str_extract(fulldata, "<record_id>(.*?)</record_id>")
-    id <- removeTAG(id)
-
-    if (do.meta) {
-    url <- str_extract(fulldata, "<source_id>(.*?)</source_id>")
-    url <- removeTAG(url)
-
-    date <- str_extract(fulldata, "<publishDate>(.*?)</publishDate>")
-    date <- removeTAG(date)
-    date <- as.Date(date, format = "%Y%m%d")
-
-    page <-  str_extract(fulldata, "<series2>(.*?)</series2>")
-    page <- removeTAG(page)
-
-    resource <-  str_extract(fulldata, "<series>(.*?)</series>")
-    resource <- removeTAG(resource)
-
-    author <-  str_extract(fulldata, "<author>(.*?)</author>")
-    author <- removeTAG(author)
-
-    leadtext <-  str_extract(fulldata, "<leadtext>(.*?)</leadtext>")
-    leadtext <- removeTAG(leadtext)
-    leadtext <- removeMISC(leadtext)
-
-    downloadDate <- str_extract(downloadDate, "[0-9-]+")
-    downloadDate <- as.Date(downloadDate, format = "%Y-%m-%d")
-    downloadDate <- rep(downloadDate, times = length(id))
-
-      mData <- data.frame(id, url, date, page, resource, author, leadtext,
-                          downloadDate, stringsAsFactors = FALSE)
-      meta <- rbind(meta, mData)
-    }
-    if (do.text) {
-    fulltext <-  str_extract(fulldata, "(<fulltext>(.*?)</fulltext>)|(<fulltext(.*?)/>)")
-    fulltext <- removeTAG(fulltext)
-    fulltext <- removeMISC(fulltext)
-    text <- c(text, fulltext)
-    names(text) <- id
-    }
-  }
-  res <- list(meta = meta, text = text)
-  class(res) <- "textmeta"
-  summary(res)
+    res <- list("meta" = meta, "text" = text, "metamult" = metamult)
+    class(res) <- "textmeta"
+    summary(res)
 }
