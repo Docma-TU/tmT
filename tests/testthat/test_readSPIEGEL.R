0f1763270a3cd534c6e9bdc19452ba79344b9a66
Lkoppers <lars.koppers@tu-dortmund.de>
2017-01-24 12:24:45 +0100

    RData 3

--- tests/testthat/test_readSPIEGEL.R
+++ tests/testthat/test_readSPIEGEL.R
@@ -6,7 +6,7 @@ SPIEGEL2 <- readSPIEGEL(path=paste0(getwd(),"/data/Spiegel"))
 text2 <- readSPIEGEL(path=paste0(getwd(),"/data/Spiegel"), do.meta = FALSE, do.text = TRUE)
 meta2 <- readSPIEGEL(path=paste0(getwd(),"/data/Spiegel"), do.meta = TRUE, do.text = FALSE)
 
-load("data/Spiegel/SP_compare.Rdata")
+load("data/SP_compare.RData")
 expect_equal(SPIEGEL2, SPIEGEL)
 expect_equal(text2, text)
 expect_equal(meta2, meta)
