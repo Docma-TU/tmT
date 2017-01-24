10165480e95ec73a60ec03b7fbd594c8525a3a51
Lkoppers <lars.koppers@tu-dortmund.de>
2017-01-24 12:18:31 +0100

    RData 2

--- tests/testthat/test_readHBWiWo.R
+++ tests/testthat/test_readHBWiWo.R
@@ -5,7 +5,7 @@ HBWiWo2 <- readHBWiWo(path=paste0(getwd(),"/data/HBWiWo"))
 text2 <- readHBWiWo(path=paste0(getwd(),"/data/HBWiWo"), do.meta = FALSE, do.text = TRUE)
 meta2 <- readHBWiWo(path=paste0(getwd(),"/data/HBWiWo"), do.meta = TRUE, do.text = FALSE)
 
-load("data/HBWiWo_compare.Rdata")
+load("data/HBWiWo_compare.RData")
 expect_equal(HBWiWo2, HBWiWo)
 expect_equal(text2, text)
 expect_equal(meta2, meta)
--- tests/testthat/test_readNexisOnline.R
+++ tests/testthat/test_readNexisOnline.R
@@ -4,8 +4,8 @@ test_that("readNexisOnline", {
   NexisOnline2 <- readNexisOnline(path = paste0(getwd(),"/data/NexisOnline"))
   text2 <- readNexisOnline(path = paste0(getwd(),"/data/NexisOnline"), do.meta = FALSE)
   meta2 <- readNexisOnline(path = paste0(getwd(),"/data/NexisOnline"), do.text = FALSE)
-  
-  load("data/NexisOnline_compare.Rdata")
+
+  load("data/NexisOnline_compare.RData")
   expect_equal(NexisOnline2, NexisOnline)
   expect_equal(text2, text)
   expect_equal(meta2, meta)
