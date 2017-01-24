47db88dde4b4f62a2471c01217013dd483ff15e7
Lkoppers <lars.koppers@tu-dortmund.de>
2017-01-24 12:51:06 +0100

    encoding new test

--- tests/testthat/test_readSPIEGEL.R
+++ tests/testthat/test_readSPIEGEL.R
@@ -1,7 +1,7 @@
 context("read SPIEGEL files")
 
 test_that("readSPIEGEL", {
-
+Sys.setlocale('LC_ALL','C')
 SPIEGEL2 <- readSPIEGEL(path=paste0(getwd(),"/data/Spiegel"))
 text2 <- readSPIEGEL(path=paste0(getwd(),"/data/Spiegel"), do.meta = FALSE, do.text = TRUE)
 meta2 <- readSPIEGEL(path=paste0(getwd(),"/data/Spiegel"), do.meta = TRUE, do.text = FALSE)
@@ -11,3 +11,8 @@ expect_equal(SPIEGEL2, SPIEGEL)
 expect_equal(text2, text)
 expect_equal(meta2, meta)
 })
+
+## SPIEGEL <- SPIEGEL2
+## text <- text2
+## meta <- meta2
+## save(SPIEGEL, text, meta, file="data/SP_compare.RData")
