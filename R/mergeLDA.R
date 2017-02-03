0f98467df4a22fb79efd37a61d0f4e222b50242e
Lkoppers <lars.koppers@tu-dortmund.de>
2017-02-03 14:42:02 +0100

    match.cluster -> mergeLDA incl. tests

---
+++ R/mergeLDA.R
@@ -0,0 +1,32 @@
+#' Preparation of different LDAs For Clustering
+#'
+#' Merges different lda-results to one matrix, including only the words which
+#' appears in all lda.
+#'
+#'
+#' @param x A list of lda results.
+#' @return A matrix including all topics from all lda-results. Number of rows
+#' is the number of topics, the number of columns is the number of words which
+#' appear in all results.
+#' @author Lars Koppers (<koppers@@statistik.tu-dortmund.de>)
+#' @keywords manip
+#' @examples
+#'
+#' ##---- Should be DIRECTLY executable !! ----
+#' @export mergeLDA
+mergeLDA <- function(x){
+    if(is.null(names(x))) names <- as.character(seq_along(x)) else names <- names(x)
+    ntimes <- sapply(x, function(y)nrow(y$topics))
+    n <- length(x)
+    x <- lapply(x,function(x)x$topics)
+    vocab <- table(unlist(sapply(x,colnames)))
+    vocab <- names(vocab)[which(vocab==n)]
+    mtch <- lapply(x,function(x)match(vocab, colnames(x)))
+    res <- NULL
+    for(i in 1:length(x))res <- rbind(res,x[[i]][,mtch[[i]]])
+    res <- res/rowSums(res)
+    rownames(res) <- paste(rep(names, ntimes), unlist(apply(as.matrix(ntimes), 1, seq)), sep="_")
+    return(res)
+}
+
+
--- global.R
+++ global.R
@@ -22,3 +22,14 @@ system("Rcmd build tmT --resave-data")
 
 
 system("Rcmd INSTALL tmT_0.1.tar.gz")
+
+#########################
+## Install from github ##
+#########################
+
+library(devtools)
+dev_mode(on=T)
+install_github("DoCMA-TU/tmT")
+library(tmT)
+dev_mode(on=F)
+
