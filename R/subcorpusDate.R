68153ba4f9491171b05660a845ddf8451f99f49f
JonasRieger <riegerjonas@gmx.de>
2017-06-23 18:18:28 +0200

    fix subcropusDate
    documents with NA as date are not returned, possible to hand over
    textmeta object

--- R/subcorpusDate.R
+++ R/subcorpusDate.R
@@ -2,23 +2,36 @@
 #'
 #' Creates a subcorpus including a specific time span.
 #'
-#'
-#' @param text List of article texts
-#' @param meta Dataframe of metadata
+#' @param object \code{\link{textmeta}} object
+#' @param text not necassary if \code{object} is specified, else should be
+#' \code{object\$text}
+#' @param meta not necassary if \code{object} is specified, else should be
+#' \code{object\$meta}
 #' @param s.date Start date of subcorpus as date object
 #' @param e.date End date of subcorpus as date object
-#' @return Filtered list of texts.
+#' @return \code{\link{textmeta}} object if \code{object} is specified,
+#' else only the filtered \code{text}.
 #' @keywords manip
 #' @examples
 #'
 #' ##---- Should be DIRECTLY executable !! ----
 #' @export subcorpusDate
-subcorpusDate <- function(text, meta, s.date = min(meta$date), e.date = max(meta$date)){
-    stopifnot((is.list(text) || is.character(text)), is.data.frame(meta),
-              length(s.date) == 1, length(e.date) == 1)
-    mtch <- match(names(text), meta$id)
-    dateID <- meta$date[mtch] >= s.date & meta$date[mtch] <= e.date
-    text <- text[dateID]
-    return(text)
+subcorpusDate <- function(object, text = object$text, meta = object$meta,
+  s.date = min(meta$date, na.rm = TRUE),
+  e.date = max(meta$date, na.rm = TRUE)){
+  
+  returnTextmeta <- TRUE
+  if(missing(object)){
+    object <- textmeta(meta = meta, text = text)
+    returnTextmeta <- FALSE
+  }
+  stopifnot(is.textmeta(object), length(s.date) == 1, length(e.date) == 1)
+  object$meta <- object$meta[match(names(object$text), object$meta$id),]
+  ind <- object$meta$date >= s.date & object$meta$date <= e.date
+  ind[is.na(ind)] <- FALSE
+  object$meta <- object$meta[ind, ]
+  object$text <- object$text[match(object$meta$id, names(object$text))]
+  if(returnTextmeta) return(object)
+  return(object$text)
 }
 
--- tests/testthat/test_subcorpusDate.R
+++ tests/testthat/test_subcorpusDate.R
@@ -4,10 +4,17 @@ test_that("subcorpusDate", {
     text <- as.list(paste("text", 1:200))
     names(text) <- paste("id", 101:300)
     set.seed(24601)
-    meta <- data.frame(id=paste("id", 1:400), date=as.Date(1:400, origin="1990-10-03"), stringsAsFactors = FALSE)
+    meta <- data.frame(
+      id = paste("id", 1:400),
+      date = as.Date(1:400, origin="1990-10-03"),
+      title = as.character(NA),
+      stringsAsFactors = FALSE)
 
-    expect_equal(text, subcorpusDate(text=text, meta=meta, s.date = min(meta$date),  e.date = max(meta$date)))
-    expect_equal(text[17:22], subcorpusDate(text=text, meta=meta, s.date = as.Date("1991-01-28"),  e.date = as.Date("1991-02-02")))
-    expect_equal(text, subcorpusDate(text=text, meta=meta, s.date = as.Date("1960-01-28"),  e.date = as.Date("1999-02-02")))
-
+    expect_equal(text, subcorpusDate(text = text, meta = meta,
+      s.date = min(meta$date), e.date = max(meta$date)))
+    expect_equal(text[17:22], subcorpusDate(text = text, meta = meta,
+      s.date = as.Date("1991-01-28"),  e.date = as.Date("1991-02-02")))
+    expect_equal(text, subcorpusDate(text = text, meta = meta,
+      s.date = as.Date("1960-01-28"),  e.date = as.Date("1999-02-02")))
+    expect_true(is.textmeta(subcorpusDate(textmeta(text = text, meta = meta))))
 })
