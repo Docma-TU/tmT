2f3c6c4abe607b1da0adf4ca5eb54555547eecc6
lkoppers <lars.koppers@tu-dortmund.de>
2017-07-17 20:27:08 +0200

    update Tests sedimentPlot

--- tests/testthat/test_sedimentPlot.R
+++ tests/testthat/test_sedimentPlot.R
@@ -14,8 +14,8 @@ test_that("sedimentPlot", {
 
     load("data/SedimentPlot.RData")
 
-    expect_equal(sP1, sedimentPlot(x=x, ldaID=ldaID, usedTopics=NULL, label=NULL, threshold=NULL, meta=meta1, unit="month", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.2))
-    expect_equal(sP2, sedimentPlot(x=x, ldaID=ldaID, usedTopics=c(2,4,7), label=NULL, threshold=NULL, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0))
+    expect_equal(sP1, sedimentPlot(x=x, ldaID=ldaID, usedTopics=NULL, label=NULL, threshold=NULL, meta=meta1, unit="month", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.08))
+    expect_equal(sP2, sedimentPlot(x=x, ldaID=ldaID, usedTopics=c(2,4,7), label=NULL, threshold=NULL, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.2))
     expect_equal(sP2, sedimentPlot(x=x, ldaID=ldaID, usedTopics=c("2", "4", "7"), label=as.character(1:10), threshold=NULL, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0))
     expect_equal(sP3, sedimentPlot(x=x, ldaID=ldaID, usedTopics=NULL, label=NULL, threshold=0.14, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0))
     expect_error(sedimentPlot(x=x, ldaID=paste("ID", 1011:2010), usedTopics=NULL, label=NULL, threshold=NULL, meta=meta1, unit="month", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.2))
