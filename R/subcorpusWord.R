0e0379da8b83c1c63c47c3efe5e62f409eb1ff8f
Lkoppers <lars.koppers@tu-dortmund.de>
2017-07-18 11:31:40 +0200

    Update subcorpusWord now allows character strings in word: pattern, word, left and right.

--- R/subcorpusWord.R
+++ R/subcorpusWord.R
@@ -5,7 +5,7 @@
 #'
 #' @param text List of article texts
 #' @param search List of data frames. Every List element is an 'or'
-#' link, every entry in a data frame is linked by an 'and'. The dataframe must have following tree variables: \code{pattern} a character string including the search terms, \code{word}, a logical value displying if a word (TRUE) or character (search) is wanted and \code{count} an integer marking how many times the word must at least be found in the text.
+#' link, every entry in a data frame is linked by an 'and'. The dataframe must have following tree variables: \code{pattern} a character string including the search terms, \code{word}, a logical value displying if a word (TRUE) or character (search) is wanted and \code{count} an integer marking how many times the word must at least be found in the text. \code{word} can alternatively be a character string containing the keywords \code{pattern} for character search, \code{word} for word-search and \code{left} and \code{right} for truncated search.
 #' If \code{wordlist} is only a character Vector the link is 'or', and a character search will be used with \code{count=1}
 #' @param ignore.case logical. lower and upper case will be ignored.
 #' @param out Type of output: \code{text} filtered corpus, \code{bin} logical vector for all texts, \code{count} the number of matches (max one match per character string).
@@ -32,13 +32,17 @@ subcorpusWord <- function(text, search, ignore.case = FALSE,
     if(is.data.frame(search)) search <- list(search)
     subid <- integer(length(text))
     counts_out <- NULL
-    
+
     for(i in 1:length(search)){
         search[[i]]$pattern <- as.character(search[[i]]$pattern)
         pattern <- search[[i]]$pattern
         if(ignore.case) search[[i]]$pattern <- tolower(search[[i]]$pattern)
-        search[[i]]$pattern[search[[i]]$word] <- paste0("\\b",search[[i]]$pattern[search[[i]]$word], "\\b")
+        if(is.logical(search[[i]]$word))search[[i]]$word <- c("pattern", "word")[search[[i]]$word+1]
+        search[[i]]$pattern[search[[i]]$word == "word"] <- paste0("\\b",search[[i]]$pattern[search[[i]]$word == "word"], "\\b")
+        search[[i]]$pattern[search[[i]]$word == "left"] <- paste0("\\b",search[[i]]$pattern[search[[i]]$word == "left"])
+        search[[i]]$pattern[search[[i]]$word == "right"] <- paste0(search[[i]]$pattern[search[[i]]$word == "right"], "\\b")
         counts <- NULL
+
         for(j in 1:nrow(search[[i]])){
             count <- lapply(text, stringr::str_count, pattern= search[[i]]$pattern[j])
             count <- sapply(count, sum)
@@ -48,9 +52,11 @@ subcorpusWord <- function(text, search, ignore.case = FALSE,
         subcandidate <- apply(counts, 1, function(x)all(x>=search[[i]]$count))
 
         subid <- subid + subcandidate
-        if(out[1] == "count"){colnames(counts) <- pattern
+        if(out[1] == "count"){#colnames(counts) <- pattern
                                      if(ignore.case)colnames(counts) <- paste0(colnames(counts), "_case")
-                                     colnames(counts)[search[[i]]$word] <- paste0(colnames(counts)[search[[i]]$word], "_w")
+                                     colnames(counts)[search[[i]]$word == "word"] <- paste0(colnames(counts)[search[[i]]$word == "word"], "_w")
+                                     colnames(counts)[search[[i]]$word == "left"] <- paste0(colnames(counts)[search[[i]]$word == "left"], "_l")
+                                     colnames(counts)[search[[i]]$word == "right"] <- paste0(colnames(counts)[search[[i]]$word == "right"], "_r")
                                      counts_out <- cbind(counts_out, counts)
                                      }
     }
--- tests/testthat/test_subcorpusWord.R
+++ tests/testthat/test_subcorpusWord.R
@@ -4,16 +4,16 @@ test_that("subcorpusWord", {
   text <- list("abaabcaa", "ababc", c("aba", "aabc"), c("aa", "aab", "bc"))
   wordlist1 <- c("aa", "bc")
   wordlist2 <- c("aa")
-  
+
   expect_error(subcorpusWord(counts, out = "text"))
-  
+
   counts <- c(2,1)
   res <- subcorpusWord(text, search=wordlist1, out = "bin")
   expect_equal(res, rep(TRUE, 4))
-  
+
   res <- subcorpusWord(text, list(data.frame(pattern=wordlist1, word=FALSE, count=counts)), out = "bin")
   expect_equal(res, c(TRUE, FALSE, FALSE, TRUE))
-  
+
   res <- subcorpusWord(text, search=wordlist1, out = "count")
   test <- matrix(c(2,0,1,2,1,1,1,1), nrow=4)
   colnames(test) <- c("aa","bc")
@@ -32,7 +32,7 @@ test_that("subcorpusWord", {
 
   res <- subcorpusWord(text, data.frame(pattern=wordlist2, count=2, word=FALSE), out = "text")
   expect_equal(res, list("abaabcaa", c("aa", "aab", "bc")))
-  
+
   res <- subcorpusWord(text, data.frame(pattern=wordlist2, count=3, word=FALSE), out = "text")
   expect_equal(res, list())
 
@@ -42,7 +42,7 @@ test_that("subcorpusWord", {
   expect_equal(subcorpusWord(text="a", search="A", out="text", ignore.case=TRUE), list("a"))
   expect_equal(subcorpusWord(text=list("a","A"), search="A", out="text", ignore.case=TRUE), list("a", "a"))
   expect_equal(subcorpusWord(text=c("a","A"), search="a", out="text", ignore.case=TRUE), list("a", "a"))
-  
+
   ## counts
   counts <- list(c(2,1), 3)
   text2 <- list("abc", c("a","a","b"), c("c","c","c"))
@@ -57,8 +57,24 @@ test_that("subcorpusWord", {
   text <- list("abaabcaa", "ababc", c("aba", "aabc"), c("aab", "bc"))
   search1 <- data.frame(pattern=c("aa", "bc"), word=c(TRUE, TRUE), count=1)
   expect_equal(subcorpusWord(text=text, search=search1, out="bin"), c(FALSE, FALSE, FALSE, FALSE))
-  
+
   text <- list("abaabcaa", "aab")
   search1 <- data.frame(pattern=c("aab"), word=c(TRUE), count=1)
   expect_equal(subcorpusWord(text=text, search=search1, out="bin"), c(FALSE, TRUE))
-  })
+
+  text <- list("abaabcaa", "ababc", c("aba", "aabc"), c("aa", "aab", "bc"))
+  search1 <- data.frame(pattern=c("aab", "a", "bc", "ab"), word=c(TRUE, FALSE, TRUE, TRUE), count=1)
+  tr <- matrix(c(0,0,0,1,5,2,4,4,0,0,0,1,0,0,0,0), nrow=4)
+  colnames(tr) <- c("aab_w", "a", "bc_w", "ab_w")
+  expect_equal(subcorpusWord(text=text, search=search1, out="count"),tr)
+
+  search1 <- data.frame(pattern=c("aab", "a", "bc", "ab"), word=c("word", "pattern", "right", "left"), count=1)
+  tr <- matrix(c(0,0,0,1,5,2,4,4,0,1,1,1,1,1,1,0), nrow=4)
+  colnames(tr) <- c("aab_w", "a", "bc_r", "ab_l")
+  expect_equal(subcorpusWord(text=text, search=search1, out="count"),tr)
+
+  search1 <- data.frame(pattern=c("aab", "a", "bc", "ab"), word=c("word", "pattern", "pattern", "pattern"), count=1)
+  tr <- matrix(c(0,0,0,1,5,2,4,4,1,1,1,1,2,2,2,1), nrow=4)
+  colnames(tr) <- c("aab_w", "a", "bc", "ab")
+  expect_equal(subcorpusWord(text=text, search=search1, out="count"),tr)
+})
