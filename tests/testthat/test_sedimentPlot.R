3bb9ff7450db9c46a1758a1e720d98af1633b450
Lkoppers <lars.koppers@tu-dortmund.de>
2017-07-19 11:57:20 +0200

    clear git chaos

--- tests/testthat/test_sedimentPlot.R
+++ tests/testthat/test_sedimentPlot.R
@@ -10,34 +10,21 @@ test_that("sedimentPlot", {
     ldaid2 <- paste("ID", 1:6)
     meta2 <- data.frame(id=paste("ID", 1:6), date=as.Date(c(1,32,64,96,128,160), origin="1990-10-03"))
 
-<<<<<<< Updated upstream
-    ## sP1 <- sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, tnames=NULL, threshold=NULL, meta=meta1, unit="month", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0)
-    ## sP2 <- sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=c(2,4,7), tnames=NULL, threshold=NULL, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.1)
-    ## sP3 <- sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, tnames=NULL, threshold=0.14, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.1)
-    ## save(sP1, sP2, sP3, file="data/SedimentPlot.RData")
-=======
     ## sP1 <- sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, tnames=NULL, threshold=NULL, meta=meta1, unit="month", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.08)
     ## sP2 <- sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=c(2,4,7), tnames=NULL, threshold=NULL, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.2)
     ## sP3 <- sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, tnames=NULL, threshold=0.14, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.1)
     ## sP4 <- sedimentPlot(ldaresult=ldaresult2, ldaid=ldaid2, select=NULL, tnames=NULL, threshold=NULL, meta=meta2, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.08)
     ## save(sP1, sP2, sP3, sP4, file="data/SedimentPlot.RData")
->>>>>>> Stashed changes
+
 
     load("data/SedimentPlot.RData")
 
     expect_equal(sP1, sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, tnames=NULL, threshold=NULL, meta=meta1, unit="month", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.08))
     expect_equal(sP2, sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=c(2,4,7), tnames=NULL, threshold=NULL, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.2))
-<<<<<<< Updated upstream
-    expect_equal(sP2, sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=c("2", "4", "7"), tnames=as.character(1:10), threshold=NULL, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0))
-    expect_equal(sP3, sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, tnames=NULL, threshold=0.14, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0))
-    expect_error(sedimentPlot(ldaresult=ldaresult, ldaid=paste("ID", 1011:2010), select=NULL, tnames=NULL, threshold=NULL, meta=meta1, unit="month", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.2))
-    
-=======
     expect_equal(sP2, sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=c("T2", "T4", "T7"), tnames=paste0("T",as.character(1:10)), threshold=NULL, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0))
     expect_equal(sP3, sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, tnames=NULL, threshold=0.14, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0))
     expect_equal(sP4, sedimentPlot(ldaresult=ldaresult2, ldaid=ldaid2, select=NULL, tnames=NULL, threshold=NULL, meta=meta2, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.08))
     expect_error(sedimentPlot(ldaresult=ldaresult, ldaid=paste("ID", 1011:2010), select=NULL, tnames=NULL, threshold=NULL, meta=meta1, unit="month", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.2))
 
->>>>>>> Stashed changes
     })
 
