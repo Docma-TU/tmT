71c6f22d931d65b11a1fb81c96df34c4c84b12d6
JonasRieger <riegerjonas@gmx.de>
2017-07-19 10:51:52 +0200

    fix test plotSed

--- tests/testthat/test_sedimentPlot.R
+++ tests/testthat/test_sedimentPlot.R
@@ -7,18 +7,18 @@ test_that("sedimentPlot", {
     ldaid <- paste("ID", 11:1010)
     meta1 <- data.frame(id=paste("ID", 1:1020), date=as.Date(sample(1:730, 1020, replace=TRUE), origin="1990-10-03"))
 
-    ## sP1 <- sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, label=NULL, threshold=NULL, meta=meta1, unit="month", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0)
-    ## sP2 <- sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=c(2,4,7), label=NULL, threshold=NULL, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.1)
-    ## sP3 <- sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, label=NULL, threshold=0.14, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.1)
+    ## sP1 <- sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, tnames=NULL, threshold=NULL, meta=meta1, unit="month", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0)
+    ## sP2 <- sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=c(2,4,7), tnames=NULL, threshold=NULL, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.1)
+    ## sP3 <- sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, tnames=NULL, threshold=0.14, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.1)
     ## save(sP1, sP2, sP3, file="data/SedimentPlot.RData")
 
     load("data/SedimentPlot.RData")
 
-    expect_equal(sP1, sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, label=NULL, threshold=NULL, meta=meta1, unit="month", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.08))
-    expect_equal(sP2, sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=c(2,4,7), label=NULL, threshold=NULL, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.2))
-    expect_equal(sP2, sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=c("2", "4", "7"), label=as.character(1:10), threshold=NULL, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0))
-    expect_equal(sP3, sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, label=NULL, threshold=0.14, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0))
-    expect_error(sedimentPlot(ldaresult=ldaresult, ldaid=paste("ID", 1011:2010), select=NULL, label=NULL, threshold=NULL, meta=meta1, unit="month", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.2))
+    expect_equal(sP1, sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, tnames=NULL, threshold=NULL, meta=meta1, unit="month", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.08))
+    expect_equal(sP2, sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=c(2,4,7), tnames=NULL, threshold=NULL, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.2))
+    expect_equal(sP2, sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=c("2", "4", "7"), tnames=as.character(1:10), threshold=NULL, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0))
+    expect_equal(sP3, sedimentPlot(ldaresult=ldaresult, ldaid=ldaid, select=NULL, tnames=NULL, threshold=0.14, meta=meta1, unit="month", xunit="month", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0))
+    expect_error(sedimentPlot(ldaresult=ldaresult, ldaid=paste("ID", 1011:2010), select=NULL, tnames=NULL, threshold=NULL, meta=meta1, unit="month", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0.2))
     
     })
 
