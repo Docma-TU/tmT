e7da6a65a474026b211fc1cb228a74b8ae0ed610
JonasRieger <riegerjonas@gmx.de>
2017-11-01 12:11:32 +0100

    maintain sedimentPlot (ldaID, file)

--- R/sedimentPlot.R
+++ R/sedimentPlot.R
@@ -4,7 +4,7 @@
 #'
 #'
 #' @param ldaresult LDA result object
-#' @param ldaid Character vector including IDs of the texts
+#' @param ldaID Character vector including IDs of the texts
 #' @param select Selects all topics if parameter is null. Otherwise vector of integers or topic label. Only topics belonging to that numbers, and labels respectively would be plotted.
 #' @param tnames Character vector of topic labels. Must have same length than number of topics in the model.
 #' @param threshold Numeric treshold between 0 and 1. Topics would only be used if at least one time unit exist with a topic proportion abov the treshold
@@ -16,6 +16,7 @@
 #' @param legend Poisition of legend. If \code{NULL} (default), no legend will be plotted
 #' @param legendLimit numeric between 0 (default) and 1. Only Topics with proportions abov this limit appear in the legend.
 #' @param peak numeric between 0 (default) and 1. Label peaks abov \code{peak}. For each Topic every area which are at least once above \code{peak} will e labeled. An area ends if the topic proportion is under 1 percent.
+#' @param file \code{character} file path if a pdf should be created
 #' @return list of two matrices. \code{x} contains the topic proportions over time, \code{y} contains the cumulated topic proportions
 #' @keywords ~kwd1 ~kwd2
 #' @examples
@@ -23,83 +24,88 @@
 #' ##---- Should be DIRECTLY executable !! ----
 #' @export sedimentPlot
 
-sedimentPlot <- function(ldaresult, ldaid, select=NULL, tnames=NULL, threshold=NULL, meta, unit="quarter", xunit="year", color=NULL, sort=TRUE, legend="topleft", legendLimit=0, peak=0){
-    if(is.null(color)) color <- RColorBrewer::brewer.pal(n=12, name="Paired")[c(2*(1:6),2*(1:6)-1)]
-    if(is.null(tnames)) tnames <- paste0("T", 1:nrow(ldaresult$document_sums))
-
-    IDmatch <- match(ldaid,meta$id)
-    if(any(is.na(IDmatch))){stop("missing id's in meta")}
-    x <- as.data.frame(t(ldaresult$document_sums))
-    textDate <- lubridate::floor_date(meta$date[IDmatch], unit)
-    x <- split(x=x, f=textDate)
-    x <- sapply(x,colSums)
-    x <- t(t(x)/colSums(x))
-    rownames(x) <- tnames
-
-    ## reduce to used topics
-    if(!is.null(select)){
-        if(is.numeric(select) | is.integer(select)){x <- x[select,]
-                                                        }else{x <- x[match(select, tnames),]
-                                                          }
+sedimentPlot <- function(ldaresult, ldaID, select = NULL, tnames = NULL,
+  threshold = NULL, meta, unit = "quarter", xunit = "year", color = NULL,
+  sort = TRUE, legend = "topleft", legendLimit = 0, peak = 0, file){
+  
+  if(!missing(file)) pdf(file, width = 15, height = 8)
+  if(is.null(color)) color <- RColorBrewer::brewer.pal(n=12, name="Paired")[c(2*(1:6),2*(1:6)-1)]
+  if(is.null(tnames)) tnames <- paste0("T", 1:nrow(ldaresult$document_sums))
+  
+  IDmatch <- match(ldaID,meta$id)
+  if(any(is.na(IDmatch))){stop("missing id's in meta")}
+  x <- as.data.frame(t(ldaresult$document_sums))
+  textDate <- lubridate::floor_date(meta$date[IDmatch], unit)
+  x <- split(x=x, f=textDate)
+  x <- sapply(x,colSums)
+  x <- t(t(x)/colSums(x))
+  rownames(x) <- tnames
+  
+  ## reduce to used topics
+  if(!is.null(select)){
+    if(is.numeric(select) | is.integer(select)){x <- x[select,]
+    }else{x <- x[match(select, tnames),]
     }
-
-    ## reduce via threshold
-    if(!is.null(threshold)){x <- x[apply(x,1, function(x)any(x>=threshold)),]}
-
-
-    ## sort topics
-    if(sort){topicVolume <- rowSums(x)
-             x <- x[order(topicVolume, decreasing=FALSE),]}
-
-    ## cumsum
-    y <- apply(x,2, cumsum)
-    y <- rbind(0,y)
-
-    ## expand color vector if necessary
-    if(length(color) < nrow(y)) color <- rep(color, ceiling(nrow(y)/length(color)))[1:nrow(x)]else color <- color[1:nrow(x)]
-
-    ## plotting
-    tmplas <- par("las")
-    par(las = 2)
-    plot(NULL, xlim=range(as.Date(colnames(x))), ylim=c(0,max(y)), ylab="", xlab="", xaxt="n")
-    for(i in 1:(nrow(y)-1)){
-        polygon(x=c(as.Date(colnames(y)), rev(as.Date(colnames(y)))), c(y[i,], rev(y[i+1,])), col=color[i])
-    }
-    xvals <- seq(lubridate::floor_date(min(as.Date(colnames(y))), unit=xunit), max(as.Date(colnames(y))), by=xunit)
-    axis(side = 1, xvals, format(xvals, "%b %y"), cex.axis = .85)
-
-    ## label peaks
-    if(peak>0){
-        xPeak <- x>peak
-        xPeaklower <- x>0.01
-        for(i in 1:nrow(xPeak)){
-            wPeak <- which(xPeak[i,])
-            if(length(wPeak)==0)next
-            if(length(wPeak)==1){ text(x=as.Date(names(wPeak)),y=(y[i,wPeak] + y[i+1,wPeak])/2, labels=rownames(y)[i+1])
-                                  next}
-            wPeakNew <- NULL
-            j <- 1
-            while(j <length(wPeak)){
-                kold <- 0
-                for(k in j:length(wPeak)){
-                    if(any(x[i,(wPeak[j]+1):(wPeak[j+1]-1)]<0.01)){ wPeakNew <- c(wPeakNew, wPeak[j:k][which.max(x[i,wPeak[j:k]])])
-                                                                    kold <- k
-                                                                    j <- k+1
-                                                                    break}
-                    if(k==length(wPeak)){wPeakNew <- c(wPeakNew, wPeak[j:k][which.max(x[i,wPeak[j:k]])])
-                                         j <- k+1}
-                }
-            }
-            if(rev(wPeakNew)[1] != rev(wPeak)[1] & k==kold+1) wPeakNew <- c(wPeakNew, rev(wPeak)[1])
-            text(x=as.Date(names(wPeakNew)),y=(y[i,wPeakNew] + y[i+1,wPeakNew])/2, labels=rownames(y)[i+1])
+  }
+  
+  ## reduce via threshold
+  if(!is.null(threshold)){x <- x[apply(x,1, function(x)any(x>=threshold)),]}
+  
+  
+  ## sort topics
+  if(sort){topicVolume <- rowSums(x)
+  x <- x[order(topicVolume, decreasing=FALSE),]}
+  
+  ## cumsum
+  y <- apply(x,2, cumsum)
+  y <- rbind(0,y)
+  
+  ## expand color vector if necessary
+  if(length(color) < nrow(y)) color <- rep(color, ceiling(nrow(y)/length(color)))[1:nrow(x)]else color <- color[1:nrow(x)]
+  
+  ## plotting
+  tmplas <- par("las")
+  par(las = 2)
+  plot(NULL, xlim=range(as.Date(colnames(x))), ylim=c(0,max(y)), ylab="", xlab="", xaxt="n")
+  for(i in 1:(nrow(y)-1)){
+    polygon(x=c(as.Date(colnames(y)), rev(as.Date(colnames(y)))), c(y[i,], rev(y[i+1,])), col=color[i])
+  }
+  xvals <- seq(lubridate::floor_date(min(as.Date(colnames(y))), unit=xunit), max(as.Date(colnames(y))), by=xunit)
+  axis(side = 1, xvals, format(xvals, "%b %y"), cex.axis = .85)
+  
+  ## label peaks
+  if(peak>0){
+    xPeak <- x>peak
+    xPeaklower <- x>0.01
+    for(i in 1:nrow(xPeak)){
+      wPeak <- which(xPeak[i,])
+      if(length(wPeak)==0)next
+      if(length(wPeak)==1){ text(x=as.Date(names(wPeak)),y=(y[i,wPeak] + y[i+1,wPeak])/2, labels=rownames(y)[i+1])
+        next}
+      wPeakNew <- NULL
+      j <- 1
+      while(j <length(wPeak)){
+        kold <- 0
+        for(k in j:length(wPeak)){
+          if(any(x[i,(wPeak[j]+1):(wPeak[j+1]-1)]<0.01)){ wPeakNew <- c(wPeakNew, wPeak[j:k][which.max(x[i,wPeak[j:k]])])
+          kold <- k
+          j <- k+1
+          break}
+          if(k==length(wPeak)){wPeakNew <- c(wPeakNew, wPeak[j:k][which.max(x[i,wPeak[j:k]])])
+          j <- k+1}
         }
+      }
+      if(rev(wPeakNew)[1] != rev(wPeak)[1] & k==kold+1) wPeakNew <- c(wPeakNew, rev(wPeak)[1])
+      text(x=as.Date(names(wPeakNew)),y=(y[i,wPeakNew] + y[i+1,wPeakNew])/2, labels=rownames(y)[i+1])
     }
-
-    ## legend
-    if(!is.null(legend)){legend(x=legend, legend= rev(rownames(y)[-1][apply(x,1,function(z)any(z>legendLimit))]), bg="white", pch=15, col=rev(color[apply(x,1,function(z)any(z>legendLimit))]))}
-    par(las=tmplas)
-    x <- data.frame(t(x))
-    y <- data.frame(t(y))[, -1]
-    invisible(list(x=x,y=y))
+  }
+  
+  ## legend
+  if(!is.null(legend)){legend(x=legend, legend= rev(rownames(y)[-1][apply(x,1,function(z)any(z>legendLimit))]), bg="white", pch=15, col=rev(color[apply(x,1,function(z)any(z>legendLimit))]))}
+  par(las=tmplas)
+  x <- data.frame(t(x))
+  y <- data.frame(t(y))[, -1]
+  if(!missing(file)) dev.off()
+  invisible(list(x=x,y=y))
 }
 
