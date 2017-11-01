75e820a3a2c7cab3faca6dc5b51771cb02e76f61
JonasRieger <riegerjonas@gmx.de>
2017-11-01 12:38:11 +0100

    maintain makeClear (default: sw="en", paragraph=FALSE)

--- R/makeClear.R
+++ R/makeClear.R
@@ -28,49 +28,53 @@
 #'
 #' ##---- Should be DIRECTLY executable !! ----
 #' @export makeClear
-makeClear <- function(object, text, sw = c(tm::stopwords("german"), "dass", "fuer", "koennen",
-                              "koennte", "ueber", "waehrend", "wuerde",
-                              "wuerden"), paragraph = TRUE){
-    returnTextmeta <- FALSE
-    if(!missing(object)){
-      text <- object$text
-      returnTextmeta <- TRUE
-    }
-    stopifnot(is.list(text), is.character(sw), is.logical(paragraph),
-              length(paragraph) == 1)
-    cat("punctuation \n")
-    text <- lapply(text, tm::removePunctuation, preserve_intra_word_dashes = FALSE)
-    cat("numbers \n")
-    text <- lapply(text, tm::removeNumbers)
-    cat("to lower \n")
-    text <- lapply(text, tolower)
-    cat("stopwords \n")
-    text <- lapply(text, tm::removeWords, sw)
-    cat("whitespace \n")
-    text <- lapply(text, tm::stripWhitespace)
-    text <- lapply(text, trimws)
-    if(paragraph){ ## SPIEGEL
-        cat("tokenization \n")
-        text <- lapply(text, strsplit, "\\s")
-        cat("remove empty article \n")
-        cat(paste("remove empty articles:",sum(lengths(text) == 0 | is.na(text)), "\n"))
-        text <- text[!(lengths(text) == 0 | is.na(text))]
-        cat("remove empty paragraphs \n")
-        text <- lapply(text, function(x) x[!(lengths(x) == 0 | is.na(x))])
-        cat(paste("remove empty articles (2):",sum(lengths(text) == 0), "\n"))
-        text <- text[!(lengths(text) == 0)]
-    }
-    else{
-        cat("tokenization \n")
-        text <- sapply(text, function(x) strsplit(x, "\\s")[1])
-        cat("remove empty article \n")
-        cat(paste("Empty Articles:",sum(lengths(text) == 0 | is.na(text)), "\n"))
-        text <- text[!(lengths(text) == 0 | is.na(text))]
-    }
-    if(returnTextmeta){
-      object$text <- text
-      object$meta <- object$meta[object$meta$id %in% names(object$text), ]
-      return(object)
-    }
-    return(text)
+makeClear <- function(object, text, sw = "en", paragraph = FALSE){
+  
+  if(sw %in% c("de", "ge", "german"))
+    sw <- c(tm::stopwords("german"), "dass", "fuer", "koennen", "koennte",
+      "ueber", "waehrend", "wuerde", "wuerden")
+  else sw <- tm::stopwords(sw)
+  
+  returnTextmeta <- FALSE
+  if(!missing(object)){
+    text <- object$text
+    returnTextmeta <- TRUE
+  }
+  stopifnot(is.list(text), is.character(sw), is.logical(paragraph),
+    length(paragraph) == 1)
+  cat("punctuation \n")
+  text <- lapply(text, tm::removePunctuation, preserve_intra_word_dashes = FALSE)
+  cat("numbers \n")
+  text <- lapply(text, tm::removeNumbers)
+  cat("to lower \n")
+  text <- lapply(text, tolower)
+  cat("stopwords \n")
+  text <- lapply(text, tm::removeWords, sw)
+  cat("whitespace \n")
+  text <- lapply(text, tm::stripWhitespace)
+  text <- lapply(text, trimws)
+  if(paragraph){ ## SPIEGEL
+    cat("tokenization \n")
+    text <- lapply(text, strsplit, "\\s")
+    cat("remove empty article \n")
+    cat(paste("remove empty articles:",sum(lengths(text) == 0 | is.na(text)), "\n"))
+    text <- text[!(lengths(text) == 0 | is.na(text))]
+    cat("remove empty paragraphs \n")
+    text <- lapply(text, function(x) x[!(lengths(x) == 0 | is.na(x))])
+    cat(paste("remove empty articles (2):",sum(lengths(text) == 0), "\n"))
+    text <- text[!(lengths(text) == 0)]
+  }
+  else{
+    cat("tokenization \n")
+    text <- sapply(text, function(x) strsplit(x, "\\s")[1])
+    cat("remove empty article \n")
+    cat(paste("Empty Articles:",sum(lengths(text) == 0 | is.na(text)), "\n"))
+    text <- text[!(lengths(text) == 0 | is.na(text))]
+  }
+  if(returnTextmeta){
+    object$text <- text
+    object$meta <- object$meta[object$meta$id %in% names(object$text), ]
+    return(object)
+  }
+  return(text)
 }
