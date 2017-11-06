3666a9c0f966d6e4f8a7ce25e002d585090cf170
JonasRieger <riegerjonas@gmx.de>
2017-11-06 13:43:25 +0100

    docLDA: stopifnot

--- R/docLDA.R
+++ R/docLDA.R
@@ -24,25 +24,26 @@
 #' @export docLDA
 
 docLDA <- function(text, vocab, ldacorrect = TRUE, excludeNA = TRUE,
-                   reduce = TRUE){
-    stopifnot(is.list(text), is.character(vocab), is.logical(ldacorrect),
-              is.logical(excludeNA), is.logical(reduce), length(ldacorrect) == 1,
-              length(excludeNA) == 1, length(reduce) == 1)
-    text <- lapply(text, unlist)
-    text <- lapply(text, table)
-    text <- lapply(text, function(x)
-      rbind(as.integer(match(names(x), vocab) - 1), as.integer(x)))
-    if(ldacorrect) text <- lapply(text, function(x)
-      rbind(as.integer(rep(x[1, ], x[2, ])), as.integer(rep(1, sum(x[2, ])))))
-    if(excludeNA) text <- lapply(text, function(x) x[, !is.na(x[1, ])])
+  reduce = TRUE){
+  
+  stopifnot(is.textmeta(textmeta(text = text)), is.character(vocab),
+    is.logical(ldacorrect), is.logical(excludeNA), is.logical(reduce),
+    length(ldacorrect) == 1, length(excludeNA) == 1, length(reduce) == 1)
+  text <- lapply(text, unlist)
+  text <- lapply(text, table)
+  text <- lapply(text, function(x)
+    rbind(as.integer(match(names(x), vocab) - 1), as.integer(x)))
+  if(ldacorrect) text <- lapply(text, function(x)
+    rbind(as.integer(rep(x[1, ], x[2, ])), as.integer(rep(1, sum(x[2, ])))))
+  if(excludeNA) text <- lapply(text, function(x) x[, !is.na(x[1, ])])
+  tmp <- lengths(lapply(text, dim)) == 0
+  text[tmp] <- lapply(text[tmp], as.matrix)
+  if(reduce){
+    # delete entries where dimension is not computable
     tmp <- lengths(lapply(text, dim)) == 0
-    text[tmp] <- lapply(text[tmp], as.matrix)
-    if(reduce){
-      # delete entries where dimension is not computable
-      tmp <- lengths(lapply(text, dim)) == 0
-      if (length(tmp) > 0) text <- text[!tmp]
-      # reducing
-      text <- text[sapply(text, dim)[2,] != 0]
-    }
-    return(text)
+    if (length(tmp) > 0) text <- text[!tmp]
+    # reducing
+    text <- text[sapply(text, dim)[2,] != 0]
+  }
+  return(text)
 }
