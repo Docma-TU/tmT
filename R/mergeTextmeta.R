6ede576fc911c290eb7fbad1d70de7a14a778d0d
JonasRieger <riegerjonas@gmx.de>
2017-11-19 17:27:34 +0100

    mergeTextmeta: argument list

--- R/mergeTextmeta.R
+++ R/mergeTextmeta.R
@@ -1,14 +1,13 @@
 #' Merge textmeta objects
 #'
-#' Merges two textmeta objects to one. It is possible to control whether all
+#' Merges a list of textmeta objects to one. It is possible to control whether all
 #' columns or the intersect should be considered.
 #'
 #'
-#' @param t1 \code{\link{textmeta}} object
-#' @param t2 \code{\link{textmeta}} object
+#' @param x a list of \code{\link{textmeta}} objects
 #' @param all \code{logical} (default: \code{TRUE}) should the result contain
 #' \code{\link{union}} (\code{TRUE}) or \code{\link{intersect}} of columns of
-#' \code{t1} and \code{t2}, if \code{TRUE} columns which only appears in one of
+#' all objects, if \code{TRUE} columns which only appears in one of
 #' the meta components the merged meta component is filled with \code{NA}s 
 #' @return \code{\link{textmeta}} object
 #' @keywords manip
@@ -17,25 +16,48 @@
 #' @export mergeTextmeta
 #'
 
-mergeTextmeta <- function(t1, t2, all = TRUE){
-  stopifnot(is.textmeta(t1), is.textmeta(t2), is.logical(all), length(all) == 1)
-  if (all) cols <- union(colnames(t1$meta), colnames(t2$meta))
-  else cols <- intersect(colnames(t1$meta), colnames(t2$meta))
+mergeTextmeta <- function(x, all = TRUE){
+  stopifnot(is.list(x), all(sapply(x, is.textmeta)),
+    is.logical(all), length(all) == 1)
+  if (any(duplicated(unlist(lapply(x, function(y) names(y$text))))))
+    cat(paste0("NOTE: There are duplicates in the names of texts",
+      ", could result in problems with unambiguity."))
+  if (all) cols <- Reduce(union, lapply(x, function(y) colnames(y$meta)))
+  else cols <- Reduce(intersect, lapply(x, function(y) colnames(y$meta)))
+  meta = NULL
   if (length(cols) > 0){
-    if (is.null(t1$meta)){
-      if (is.null(t2$meta)) meta = NULL
-      else meta = t2$meta[, cols]
+    ind <- which(sapply(x, function(y) is.data.frame(y$meta)))
+    mymerge <- function(t1, t2)
+      merge(t1$meta, t2$meta, all = TRUE, sort = FALSE)[, cols]
+    # does not work: if (length(ind) > 1) meta <- Reduce(mymerge, x[ind])
+    if (length(ind) > 1){
+      meta <- (x[[ind[1]]])$meta
+      for (i in 2:length(ind))
+        meta <- merge(meta, (x[[ind[i]]])$meta, all = TRUE, sort = FALSE)[, cols]
     }
-    else{
-      if (is.null(t2$meta)) meta = t1$meta[, cols]
-      else meta = merge(t1$meta, t2$meta, all = TRUE, sort = FALSE)[, cols]
-    }
+    else meta <- (x[[ind]])$meta # only one data.frame
   }
-  else meta = NULL
-  object <-
-    textmeta(
-      meta = meta,
-      text = c(t1$text, t2$text),
-      metamult = c(t1$metamult, t2$metamult))
+  object <- textmeta(meta = meta, text = Reduce(c, lapply(x, function(y) y$text)),
+    metamult = Reduce(c, lapply(x, function(y) y$metamult)))
   return(object)
 }
+
+# mergeTextmeta <- function(t1, t2, all = TRUE){
+#   stopifnot(is.textmeta(t1), is.textmeta(t2), is.logical(all), length(all) == 1)
+#   if (all) cols <- union(colnames(t1$meta), colnames(t2$meta))
+#   else cols <- intersect(colnames(t1$meta), colnames(t2$meta))
+#   if (length(cols) > 0){
+#     if (is.null(t1$meta)){
+#       if (is.null(t2$meta)) meta = NULL
+#       else meta = t2$meta[, cols]
+#     }
+#     else{
+#       if (is.null(t2$meta)) meta = t1$meta[, cols]
+#       else meta = merge(t1$meta, t2$meta, all = TRUE, sort = FALSE)[, cols]
+#     }
+#   }
+#   else meta = NULL
+#   object <- textmeta(meta = meta, text = c(t1$text, t2$text),
+#     metamult = c(t1$metamult, t2$metamult))
+#   return(object)
+# }
--- tests/testthat/test_mergeTextmeta.R
+++ tests/testthat/test_mergeTextmeta.R
@@ -7,12 +7,15 @@ test_that("mergeTextmeta", {
     meta = data.frame(id = c("a", "b", "y"),
       date = as.Date(c("2017-01-01", "2017-01-01", "2017-01-01")),
       title = c("abc", "bed", "yxy"), stringsAsFactors = FALSE))
-  m1 <- mergeTextmeta(abc, abc)
-  m2 <- mergeTextmeta(abc, abc2)
-  m3 <- mergeTextmeta(abc2, abc)
-  m4 <- mergeTextmeta(abc2, abc2)
+  m1 <- mergeTextmeta(list(abc, abc))
+  m2 <- mergeTextmeta(list(abc, abc2))
+  m3 <- mergeTextmeta(list(abc2, abc))
+  m4 <- mergeTextmeta(list(abc2, abc2))
+  m5 <- mergeTextmeta(list(abc, abc, abc2, abc, abc, abc2, abc2))
+  expect_true(is.textmeta(m5))
+  expect_equal(length(m5$text), 4*length(abc$text)+3*length(abc2$text))
   expect_true(
     all(is.textmeta(m1), is.textmeta(m2), is.textmeta(m3), is.textmeta(m4)))
-  expect_error(mergeTextmeta(abc, NULL))
-  expect_equal(mergeTextmeta(abc, abc2, all = FALSE), m1)
+  expect_error(mergeTextmeta(abc, abc))
+  expect_equal(mergeTextmeta(list(abc, abc2), all = FALSE), m1)
 })
