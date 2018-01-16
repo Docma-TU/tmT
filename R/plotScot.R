22b3a8c24d6e06c652ca4453cccaf9a78299586a
karinboczek <karin.boczek@tu-dortmund.de>
2018-01-16 15:05:08 +0100

    Update Help plotScot.R
    Details for needed object: type

--- R/plotScot.R
+++ R/plotScot.R
@@ -29,6 +29,7 @@
 #' to zeros. Only has effect if \code{rel = TRUE}.
 #' @param file \code{character} file path if a pdf should be created
 #' @param ... additional graphical parameters 
+#' @details \code{object} needs a	textmeta object with strictly tokenized text component (character vectors) if you use \code{type = "words}. If you use \code{type = "docs"} use can use a tokenized or a not tokenized text component.
 #' @return A plot.
 #' Invisible: A dataframe with columns \code{date} and \code{counts},
 #' respectively \code{proportion}
