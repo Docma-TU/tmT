8fececdf1525486f99b4ae4f22ad43504d947750
JonasRieger <riegerjonas@gmx.de>
2018-01-28 15:01:58 +0100

    possible matrix as meta, add dateFormat

--- R/textmeta.R
+++ R/textmeta.R
@@ -2,17 +2,22 @@
 #'
 #' Creates "textmeta"-Objects.
 #'
-#' @param meta data.frame of the meta-data.
-#' @param text list or character of the text-data.
+#' @param meta data.frame (or matrix) of the meta-data.
+#' @param text list (or character vector) of the text-data.
 #' @param metamult list of the metamult-data.
+#' @param dateFormat \code{character} string with the date format in meta.
+#' for \code{\link{as.Date}}
 #' @return a "textmeta"-Object.
 #' @keywords manip
 #' @examples
 #'
 #' @export textmeta
-textmeta <- function(meta = NULL, text = NULL, metamult = NULL){
+textmeta <- function(meta = NULL, text = NULL, metamult = NULL, dateFormat = "%Y-%m-%d"){
+  meta <- as.data.frame(meta, stringsAsFactors = FALSE)
+  meta$date <- as.Date(meta$date, format = dateFormat)
+  text <- lapply(text, identity)
   stopifnot(is.null(meta) || is.data.frame(meta),
-            is.null(text) || is.list(text) || is.character(text),
+            is.null(text) || is.list(text),
             is.null(metamult) || is.list(metamult))
   res <- list(meta = meta, text = text, metamult = metamult)
   class(res) <- "textmeta"
@@ -75,37 +80,28 @@ print.textmeta <- function(x, ...){
                  round(mean(na.date), 2), ")\n"))
     }
   }
-  ## if ("datum" %in% colnames(x$meta)){
-  ##   na.date <- is.na(x$meta$datum)
-  ##   if (any(!na.date)){
-  ##     cat(paste0(" range of date: ", paste(range(x$meta$datum, na.rm = TRUE),
-  ##                                          collapse = " till "), "\n"))
-  ##   }
-  ##   if (any(na.date)){
-  ##     cat(paste0(" NAs in date: ", sum(na.date), " (", mean(na.date, 2), ")"))
-  ##   }
-  ## }
+  
   invisible(x)
 }
 
 #' @rdname textmeta
 #' @export
 #' @param object textmeta object
-#' @param list.names Character vector with names of textmeta lists (meta, text, metamult). Summaries where generated only for those lists. Default gives summaries for all lists.
-#' @param candidates Character vector with variable-names from the meta dataset. Summaries where only generated for those variables.
-summary.textmeta <- function(object, list.names = names(object),
-                             candidates = c("resource", "downloadDate"), ...){
-  stopifnot(is.textmeta(object), is.character(list.names),
-            all(list.names %in% names(object)),
-            ifelse("text" %in% list.names && length(object$text) > 0,
+#' @param listnames Character vector with names of textmeta lists (meta, text, metamult). Summaries are generated for those lists only. Default gives summaries for all lists.
+#' @param metavariables Character vector with variable-names from the meta dataset. Summaries are generated for those variables only.
+summary.textmeta <- function(object, listnames = names(object),
+                             metavariables = character(), ...){
+  stopifnot(is.textmeta(object), is.character(listnames),
+            all(listnames %in% names(object)),
+            ifelse("text" %in% listnames && length(object$text) > 0,
                    is.list(object$text) || is.character(object$text), TRUE),
-            ifelse("meta" %in% list.names && !is.null(nrow(object$meta)),
+            ifelse("meta" %in% listnames && !is.null(nrow(object$meta)),
                    is.data.frame(object$meta), TRUE),
-            ifelse("metamult" %in% list.names && !is.null(nrow(object$metamult)),
+            ifelse("metamult" %in% listnames && !is.null(nrow(object$metamult)),
                    is.list(object$metamult), TRUE))
   nextprint <- paste(paste0(rep("-", 70), collapse = ""), "\n\n")
   # object$text:
-  if ("text" %in% list.names && length(object$text) > 0){
+  if ("text" %in% listnames && length(object$text) > 0){
     n.text <- length(object$text)
     na.text <- sum(is.na(object$text))
     # print:
@@ -124,8 +120,8 @@ summary.textmeta <- function(object, list.names = names(object),
                ncol(object$meta), " variables\n"))
     cat("\nNAs in meta:\n")
     print(cbind(abs = na.meta, rel = round(na.meta/n.meta, 2)))
-    # print tables (not NA-table) of candidates
-    for (i in candidates){
+    # print tables (not NA-table) of metavariables
+    for (i in metavariables){
       if (i %in% cols){
         tab <- table(object$meta[, i])
         cat(paste0(nextprint, i, ":\n"))
@@ -145,18 +141,7 @@ summary.textmeta <- function(object, list.names = names(object),
                    round(na.meta["date"]/n.meta, 2), ")\n"))
       }
     }
-    ## if ("datum" %in% cols){
-    ##   cat(nextprint)
-    ##   if (na.meta["datum"] < n.meta){
-    ##     cat(paste0("range of date: ",
-    ##                paste(range(object$meta$datum, na.rm = TRUE),
-    ##                      collapse = " till "), "\n"))
-    ##   }
-    ##   if (na.meta["datum"] > 0){
-    ##     cat(paste0("NAs in date: ", na.meta["datum"], " (",
-    ##                round(na.meta["datum"]/n.meta, 2), ")\n"))
-    ##   }
-    ## }
+  
   }
   if ("metamult" %in% list.names && length(object$metamult) > 0){
     n.metamult <- sapply(object$metamult, function(x) sum(lengths(x)))
