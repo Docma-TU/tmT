97ae7160deac6335ac18242fb710a7b65cc9c624
JonasRieger <riegerjonas@gmx.de>
2018-01-31 18:13:27 +0100

    improve outputs

--- R/makeClear.R
+++ R/makeClear.R
@@ -12,8 +12,16 @@
 #' \code{object\$text}: list of article texts
 #' @param sw \code{character} vector of stopwords. If the vector is of length
 #' one, \code{sw} is interpretated as argument for \code{\link{stopwords}}
-#' @param paragraph Logical: Should be set to \code{TRUE} if the article is a
+#' @param paragraph Logical: Should be set to \code{TRUE} if one article is a
 #' list of character strings, representing the paragraphs.
+#' @param lowercase Logical: Should be set to \code{TRUE} if all letters should
+#' be coerced to lowercase.
+#' @param rmPunctuation Logical: Should be set to \code{TRUE} if punctuation should
+#' be removed from articles.
+#' @param rmNumbers Logical: Should be set to \code{TRUE} if numbers should
+#' be removed from articles.
+#' @param checkUTF8 Logical: Should be set to \code{TRUE} if articles should
+#' be tested on UTF-8 - which is package standard.
 #' @return A list containing the preprocessed Article.
 #' @keywords manip
 #' @import graphics
@@ -29,7 +37,8 @@
 #'
 #' ##---- Should be DIRECTLY executable !! ----
 #' @export makeClear
-makeClear <- function(object, text, sw = "en", paragraph = FALSE){
+makeClear <- function(object, text, sw = "en", paragraph = FALSE,
+  lowercase = TRUE, rmPunctuation = TRUE, rmNumbers = TRUE, checkUTF8 = TRUE){
   
   if(length(sw) == 1){
     if(sw %in% c("de", "ge", "german"))
@@ -37,7 +46,6 @@ makeClear <- function(object, text, sw = "en", paragraph = FALSE){
         "ueber", "waehrend", "wuerde", "wuerden")
     else sw <- tm::stopwords(sw)
   }
-  
   returnTextmeta <- FALSE
   if(!missing(object)){
     stopifnot(is.textmeta(object))
@@ -45,36 +53,57 @@ makeClear <- function(object, text, sw = "en", paragraph = FALSE){
     returnTextmeta <- TRUE
   }
   stopifnot(is.textmeta(textmeta(text = text)), is.character(sw),
-    is.logical(paragraph), length(paragraph) == 1)
-  cat("to lower \n")
-  text <- lapply(text, tolower)
-  cat("punctuation \n")
-  text <- lapply(text, tm::removePunctuation, preserve_intra_word_dashes = FALSE)
-  cat("numbers \n")
-  text <- lapply(text, tm::removeNumbers)
-  cat("stopwords \n")
+    is.logical(paragraph), length(paragraph) == 1,
+    all(is.logical(c(lowercase, rmPunctuation, rmNumbers))))
+  cat("Check Articles on UTF8: ")
+  if(checkUTF8){
+    stopifnot(all(unlist(lapply(text, function(x) lapply(x, validUTF8)))))
+    cat("next Step\n")
+  }
+  else cat("skip\n")
+  cat("Change to Lowercase: ")
+  if(lowercase){
+    text <- lapply(text, tolower)
+    cat("next Step\n")
+  }
+  else cat("skip\n")
+  cat("Remove Punctuation: ")
+  if(rmPunctuation){
+    text <- lapply(text, tm::removePunctuation, preserve_intra_word_dashes = FALSE)
+    cat("next Step\n")
+  }
+  else cat("skip\n")
+  cat("Remove Numbers: ")
+  if(rmNumbers){
+    text <- lapply(text, tm::removeNumbers)
+    cat("next Step\n")
+  }
+  else cat("skip\n")
+  cat("Remove Stopwords: ")
   text <- lapply(text, tm::removeWords, sw)
-  cat("whitespace \n")
+  cat("next Step\nRemove redundant Whitespace: ")
   text <- lapply(text, tm::stripWhitespace)
   text <- lapply(text, trimws)
-  if(paragraph){ ## SPIEGEL
-    cat("tokenization \n")
+  cat("next Step\n")
+  if(paragraph){
+    cat("Tokenize: ")
     text <- lapply(text, strsplit, "\\s")
-    cat("remove empty article \n")
-    cat(paste("remove empty articles:",sum(lengths(text) == 0 | is.na(text)), "\n"))
+    cat(paste("next Step\nRemove empty Articles: ",
+      sum(lengths(text) == 0 | is.na(text))))
     text <- text[!(lengths(text) == 0 | is.na(text))]
-    cat("remove empty paragraphs \n")
+    cat(" next Step\nRemove empty Paragraphs: ")
     text <- lapply(text, function(x) x[!(lengths(x) == 0 | is.na(x))])
-    cat(paste("remove empty articles (2):",sum(lengths(text) == 0), "\n"))
+    cat(paste(" next Step\nRemove empty Articles (2): ", sum(lengths(text) == 0)))
     text <- text[!(lengths(text) == 0)]
   }
   else{
-    cat("tokenization \n")
+    cat("Tokenize: ")
     text <- sapply(text, function(x) strsplit(x, "\\s")[1])
-    cat("remove empty article \n")
-    cat(paste("Empty Articles:",sum(lengths(text) == 0 | is.na(text)), "\n"))
+    cat(paste("next Step\nRemove empty Articles: ",
+      sum(lengths(text) == 0 | is.na(text))))
     text <- text[!(lengths(text) == 0 | is.na(text))]
   }
+  cat(" Success")
   if(returnTextmeta){
     object$text <- text
     object$meta <- object$meta[object$meta$id %in% names(object$text), ]
--- R/makeWordlist.R
+++ R/makeWordlist.R
@@ -15,26 +15,28 @@
 #' ##---- Should be DIRECTLY executable !! ----
 #' @export makeWordlist
 makeWordlist <- function(text, k = 100000L){
-    stopifnot(is.textmeta(textmeta(text = text)),
-      is.numeric(k), as.integer(k) == k, length(k) == 1)
-    n <- length(text)
-    cat(paste(n,"\n"))
-    cat("wordlist\n")
-    N <- 0:floor(n/k)
-    words <- NULL
-    for(i in N){
-        cat(paste(i*k, "\n"))
-        words <- c(words, unique(unlist(text[(i*k+1):(min(n, i*k+k))])))
-    }
-    words <- sort(unique(words))
-    cat("table", "\n")
-    wordtable <- rep(0, length(words))
-    names(wordtable) <- words
-    for(i in N){
-        cat(paste(i*k), "\n")
-        tmp <- table(unlist(text[(i*k+1):(min(n, i*k+k))]))
-        mtch <- match(names(tmp), names(wordtable))
-        wordtable[mtch] <- wordtable[mtch] + tmp
-    }
-    return(list(words = words, wordtable = wordtable))
+  stopifnot(is.textmeta(textmeta(text = text)),
+    is.numeric(k), as.integer(k) == k, length(k) == 1)
+  n <- length(text)
+  cat(paste("Number of Articles:", n,"\n"))
+  cat("Find out Vocabularies...\n Done:\n")
+  N <- 0:floor(n/k)
+  words <- NULL
+  for(i in N){
+    cat("  ", i*k, "\n")
+    words <- c(words, unique(unlist(text[(i*k+1):(min(n, i*k+k))])))
+  }
+  cat("  ", n, " next Step\n")
+  words <- sort(unique(words))
+  cat("Calculate Counts...\n Done:\n")
+  wordtable <- rep(0, length(words))
+  names(wordtable) <- words
+  for(i in N){
+    cat("  ", i*k, "\n")
+    tmp <- table(unlist(text[(i*k+1):(min(n, i*k+k))]))
+    mtch <- match(names(tmp), names(wordtable))
+    wordtable[mtch] <- wordtable[mtch] + tmp
+  }
+  cat("  ", n, " Success\n")
+  return(list(words = words, wordtable = wordtable))
 }
