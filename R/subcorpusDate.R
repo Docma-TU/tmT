ddd07fcced60d7a3772cce5c4f56388ea779a721
Lkoppers <lars.koppers@tu-dortmund.de>
2018-02-07 11:14:51 +0100

    example subcorpusDate, subcorpusWord update

--- R/subcorpusDate.R
+++ R/subcorpusDate.R
@@ -13,13 +13,16 @@
 #' else only the filtered \code{text}.
 #' @keywords manip
 #' @examples
-#'
-#' ##---- Should be DIRECTLY executable !! ----
+#' texts <- list(A="Give a Man a Fish, and You Feed Him for a Day. Teach a Man To Fish, and You Feed Him for a Lifetime", B="So Long, and Thanks for All the Fish", C="A very able manipulative mathematician, Fisher enjoys a real mastery in evaluating complicated multiple integrals.")
+#' corpus <- textmeta(meta=data.frame(id=c("A", "B", "C", "D"), title=c("Fishing", "Don't panic!", "Sir Ronald", "Berlin"), date=c("1885-01-02", "1979-03-04", "1951-05-06", "1967-06-02"), stringsAsFactors=FALSE), text=texts)
+#' subcorpus <- subcorpusDate(object=corpus, s.date = "1951-05-06")
+#' subcorpus$meta
+#' subcorpus$text
 #' @export subcorpusDate
 subcorpusDate <- function(object, text = object$text, meta = object$meta,
   s.date = min(meta$date, na.rm = TRUE),
   e.date = max(meta$date, na.rm = TRUE)){
-  
+
   returnTextmeta <- TRUE
   if(missing(object)){
     object <- textmeta(meta = meta, text = text)
@@ -34,4 +37,3 @@ subcorpusDate <- function(object, text = object$text, meta = object$meta,
   if(returnTextmeta) return(object)
   return(object$text)
 }
-
