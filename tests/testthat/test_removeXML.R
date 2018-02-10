7e6e2ba1822c929638f00747fecf7fdeceb5cbe0
lkoppers <lars.koppers@tu-dortmund.de>
2018-02-10 23:03:24 +0100

    next try

--- tests/testthat/test_removeXML.R
+++ tests/testthat/test_removeXML.R
@@ -4,7 +4,10 @@ test_that("removeXML", {
 #Sys.setlocale('LC_ALL','C')
 
 text <- c("aba<vcs>ab</vcs>caa", "ab&dgv;abc", "&auml;&Auml;&ouml;&Ouml;&uuml;&Uuml;&szlig;", "aa", "aab", "bc")
-text2 <- c("aba<vcs>ab</vcs>caa", "ab&dgv;abc", "\ue4\uc4\uf6\ud6\ufc\udc\udf", "aa", "aab", "bc")
+
+tmp <- c("e4","c4","f6","d6","fc","dc","df")
+tmp <- paste0(sapply(tmp, function(x)eval(parse(text = paste0("'\\u", x, "'")))), collapse = "")
+text2 <- c("aba<vcs>ab</vcs>caa", "ab&dgv;abc", tmp, "aa", "aab", "bc")
 
 expect_equal(removeXML(x=text2), c("aba ab caa","ab&dgv;abc","\UE4\UC4\UF6\UD6\UFC\UDC\UDF","aa","aab","bc"))
 expect_equal(removeHTML(x=text, symbolList = 1, dec=FALSE, hex=FALSE, delete = FALSE), text2)
