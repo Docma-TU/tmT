991a1e007236dc99f107c924ba02717b8bcec94a
lkoppers <lars.koppers@tu-dortmund.de>
2018-02-10 22:05:49 +0100

    fixed removeXML removeHTML removeUmlauts incl tests new rd files

--- tests/testthat/test_removeXML.R
+++ tests/testthat/test_removeXML.R
@@ -6,17 +6,17 @@ Sys.setlocale('LC_ALL','C')
 text <- c("aba<vcs>ab</vcs>caa", "ab&dgv;abc", "&auml;&Auml;&ouml;&Ouml;&uuml;&Uuml;&szlig;", "aa", "aab", "bc")
 text2 <- c("aba<vcs>ab</vcs>caa", "ab&dgv;abc", "\U000E4\U000C4\U000F6\U000D6\U000FC\U000DC\U000DF", "aa", "aab", "bc")
 
-removeXML(x=text2, xml=TRUE, umlauts = FALSE, u.type = c("normal", "html", "all"))
+expect_equal(removeXML(x=text2), c("aba ab caa","ab&dgv;abc","\U000E4\U000C4\U000F6\U000D6\U000FC\U000DC\U000DF","aa","aab","bc"))
+expect_equal(removeHTML(x=text, symbolList = 1, dec=FALSE, hex=FALSE, delete = FALSE), text2)
 
-expect_equal(removeXML(x=text2, xml=TRUE, umlauts = FALSE), c("aba ab caa","ab&dgv;abc","\U000E4\U000C4\U000F6\U000D6\U000FC\U000DC\U000DF","aa","aab","bc"))
-expect_equal(removeXML(x=text2, xml=FALSE, umlauts = FALSE), text2)
-
+umlauts <- c("aba ab caa","ab&dgv;abc","\U000E4\U000C4\U000F6\U000D6\U000FC\U000DC\U000DF","aa","aab","bc")
 exp1 <- c("aba ab caa", "ab&dgv;abc", "aeAeoeOeueUess", "aa", "aab", "bc")
-exp2 <- c("aba ab caa", "ababc", "aeAeoeOeueUess", "aa",  "aab", "bc")
-expect_equal(removeXML(x=text2, umlauts = TRUE, u.type = "normal"), exp1)
-expect_equal(removeXML(x=text , umlauts = TRUE, u.type = "html"), exp2)
-expect_equal(removeXML(x=text2, umlauts = TRUE, u.type = "all"), exp2)
-expect_equal(removeXML(x=text , umlauts = TRUE, u.type = "all"), exp2)
+expect_equal(removeUmlauts(x=umlauts), exp1)
+
+
+
 })
 
-
+# removeHTML(x, dec=TRUE, hex=TRUE, entity=TRUE, symbolList=1, delete=TRUE, symbols=FALSE)
+# removeUmlautsfunction(x)
+# removeXML(x)
