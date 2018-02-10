a5554bd457b10b3210632b4a4663757b4d899a33
lkoppers <lars.koppers@tu-dortmund.de>
2018-02-10 22:45:40 +0100

    Problems with lower/upper cases in Unicode?

--- tests/testthat/test_removeXML.R
+++ tests/testthat/test_removeXML.R
@@ -4,12 +4,12 @@ test_that("removeXML", {
 #Sys.setlocale('LC_ALL','C')
 
 text <- c("aba<vcs>ab</vcs>caa", "ab&dgv;abc", "&auml;&Auml;&ouml;&Ouml;&uuml;&Uuml;&szlig;", "aa", "aab", "bc")
-text2 <- c("aba<vcs>ab</vcs>caa", "ab&dgv;abc", "\UE4\UC4\UF6\UD6\UFC\UDC\UDF", "aa", "aab", "bc")
+text2 <- c("aba<vcs>ab</vcs>caa", "ab&dgv;abc", "\ue4\uc4\uf6\ud6\ufc\udc\udf", "aa", "aab", "bc")
 
 expect_equal(removeXML(x=text2), c("aba ab caa","ab&dgv;abc","\UE4\UC4\UF6\UD6\UFC\UDC\UDF","aa","aab","bc"))
 expect_equal(removeHTML(x=text, symbolList = 1, dec=FALSE, hex=FALSE, delete = FALSE), text2)
 
-umlauts <- c("aba ab caa","ab&dgv;abc","\U000E4\U000C4\U000F6\U000D6\U000FC\U000DC\U000DF","aa","aab","bc")
+umlauts <- c("aba ab caa","ab&dgv;abc","\UE4\UC4\UF6\UD6\UFC\UDC\UDF","aa","aab","bc")
 exp1 <- c("aba ab caa", "ab&dgv;abc", "aeAeoeOeueUess", "aa", "aab", "bc")
 expect_equal(removeUmlauts(x=umlauts), exp1)
 
