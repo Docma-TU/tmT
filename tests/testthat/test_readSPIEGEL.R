9274c523b92742677ec0d884047db45441cfee91
JonasRieger <riegerjonas@gmx.de>
2018-07-22 11:47:47 +0200

    fix travis

--- tests/testthat/test_readSPIEGEL.R
+++ tests/testthat/test_readSPIEGEL.R
@@ -4,9 +4,9 @@ test_that("readSPIEGEL", {
   SP2 <- readSPIEGEL(path=paste0(getwd(),"/data/Spiegel"))
   text2 <- readSPIEGEL(path=paste0(getwd(),"/data/Spiegel"), do.meta = FALSE, do.text = TRUE)
   meta2 <- readSPIEGEL(path=paste0(getwd(),"/data/Spiegel"), do.meta = TRUE, do.text = FALSE)
-  
+
   load("data/SP_compare.RData")
-  expect_equal(SP2, SPIEGEL)
+  expect_equal(SP2, SP)
   expect_equal(text2, text)
   expect_equal(meta2, meta)
 })
