9ad4b97f7060833fdf480c26169bbee5a5bb7cc1
JonasRieger <riegerjonas@gmx.de>
2018-11-28 13:50:13 +0100

    new function readWhatsApp

---
+++ R/readWhatsApp.R
@@ -0,0 +1,104 @@
+#' Read WhatsApp files
+#'
+#' Reads HTML-files from WhatsApp and separates the text and meta data.
+#'
+#' @param path Character: string with path where the data files are.
+#' @param file Character: string with names of the HTML files.
+#' @param encoding encoding of the input files.
+#' @return \code{\link{textmeta}} object.
+#' @author Jonas Rieger (<jonas.rieger@@tu-dortmund.de>)
+#' @keywords manip
+#'
+#' @export readWhatsApp
+#'
+readWhatsApp = function(path = getwd(), file = list.files(path = path,
+  pattern = "*.html$", full.names = FALSE, recursive = TRUE)){
+
+  text = NULL
+  meta = NULL
+  filenames = substr(file, 1, nchar(file)-5)
+  readTime = Sys.time()
+
+  for (i in seq_along(file)) {
+    #message("read WhatsApp-Chat \"", filenames[i], "\" ...")
+    article = readLines(con = paste(path, file[i], sep="/"), encoding = "UTF-8")
+    lines = grep(pattern = "<div class=\"vW7d1((.){7})?\">", x = article)
+
+    articlespan = article[min(lines):length(article)]
+    obs = tail(
+      unlist(strsplit(paste(articlespan, collapse = " "),
+        "<div class=\"vW7d1((.){7})?\">")), n = -1)
+    # jeweils erster Wert des Splits keine Beobachtung!
+
+    mData = data.frame(id = paste0(filenames[i], seq_along(obs)), title = NA_character_,
+      stringsAsFactors = FALSE)
+    # IDs noch genauer setzen. Iwie den Namen des Chats einbringen,
+    # siehe dafuer article[lines] vor obs
+    newtext = character(length(obs))
+
+    mData$systemActivity = grepl(pattern = "<div class=\"_3_7SH Zq3Mc tail\">", obs)
+    mData$systemDate = grepl(pattern = "<div class=\"_3_7SH Zq3Mc\">", obs)
+    mData$systemSecurity = grepl(pattern = "<div class=\"_3_7SH _14b5J Zq3Mc tail\">", obs)
+
+    systemMessage = mData$systemActivity | mData$systemDate | mData$systemSecurity
+    newtext[systemMessage] = tosca::removeXML(stringr::str_extract(obs[systemMessage],
+      "<div class=\"_3_7SH( _14b5J)? Zq3Mc( tail)?\">(.*?)</div>"))
+
+    mData$userMessage = !systemMessage
+    mData$textMessage = grepl(pattern = "<div class=\"_3_7SH _3DFk6( message-(in|out))?( tail)?\">", obs)
+    mData$imageMessage = grepl(pattern = "<div class=\"_3_7SH _3qMSo( message-(in|out))?( tail)?\">", obs)
+    mData$audioMessage = grepl(pattern = "<div class=\"_3_7SH _1gqYh( message-(in|out))?( tail)?\">", obs)
+    header = stringr::str_extract(obs[mData$userMessage],
+      "<div class=\"(.*?)copyable-text\" data-pre-plain-text=\"(.*?)\"><div")
+    header = stringr::str_extract(header,
+      "copyable-text\" data-pre-plain-text=\"(.*?)\"><div")
+    mData$posix[mData$userMessage] = substr(header, 38, 54)
+    mData$posix = trimws(gsub(pattern = "\\]", x = mData$posix, replacement = ""))
+    mData$posix = as.POSIXct(mData$posix, format = "%H:%M, %d.%m.%Y", "CET")
+    mData$date = as.Date(mData$posix)
+    mData$author[mData$userMessage] = substr(header, 55, nchar(header) - 8)
+    mData$author = trimws(gsub(pattern = "\\]", x = mData$author, replacement = ""))
+
+    textandemojis = stringr::str_extract(obs[mData$userMessage],
+      "<((span dir=\"ltr\" class=\")|(div class=\"_2x9bY ))selectable-text invisible-space copyable-text\">(.*?)</div>")
+    # emojji detection and extraction
+    textandemojis = strsplit(textandemojis, "<img crossorigin")
+    tmp = cumsum(lengths(textandemojis))
+    indsplit = rbind(c(1, head(tmp, -1)+1), tmp)
+    textandemojis = unlist(textandemojis)
+    indemoji = grepl(x = textandemojis, pattern = "data-plain-text=\"(.*?)\">")
+    tmp = stringr::str_extract(string = textandemojis[indemoji],
+      pattern = "data-plain-text=\"(.*?)\">")
+    tmp = stringr::str_extract(string = tmp, pattern = "data-plain-text=\"(.*?)\"")
+    textandemojis[indemoji] = paste(substr(tmp, 18, nchar(tmp)-1),
+      tosca::removeXML(paste("<", textandemojis[indemoji])))
+    textandemojis = sapply(seq_len(ncol(indsplit)), function(x)
+      paste(textandemojis[indsplit[1,x]:indsplit[2,x]], collapse = " "))
+
+    newtext[mData$userMessage] = tosca::removeXML(textandemojis)
+
+    mData$forward[mData$userMessage] = grepl(
+      pattern = "<span data-icon=\"forward-indicator\" class=\"ySrGA\">",
+      x = obs[mData$userMessage])
+    mData$cited[mData$userMessage] = grepl(pattern = "<span dir=\"auto\" class=\"quoted-mention\">",
+      x = obs[mData$userMessage])
+    mData$citedAuthor[mData$userMessage] = tosca::removeXML(stringr::str_extract(obs[mData$userMessage],
+      "<span dir=\"auto\" class=\"((_3Ye_R _1wjpf)|(_2a1Yw))\">(.*?)</div>"))
+    mData$citedText[mData$userMessage] = tosca::removeXML(stringr::str_extract(obs[mData$userMessage],
+      "<span dir=\"auto\" class=\"quoted-mention\">(.*?)</div>"))
+    # Zitat koennte Emojis enthalten?!
+
+    mData$time[mData$userMessage] = tosca::removeXML(stringr::str_extract(obs[mData$userMessage],
+      "<span class=\"_3EFt_\">(.*?)</span>"))
+
+    meta = rbind(meta, mData)
+
+    names(newtext) = mData$id
+    text = c(text, newtext)
+  }
+  res = tosca::textmeta(meta = meta, text = as.list(text))
+  readTime = difftime(Sys.time(), readTime)
+  cat("Time for Reading Data:", round(as.numeric(readTime), 2), attributes(readTime)$units,
+    "\n----------------------------------------------------------------------\n")
+  summary(res)
+}
